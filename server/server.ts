e4ebb97108d96d7f1471d9e6a1c156af857dbcd1
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-01-18 15:10:20 +0100

    Angi riktig path til statiske ressurser når vi kjører lokalt

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import path from "path";
+
 const basePath = "/min-ia";
-const buildPath = path.resolve(__dirname, "../../client/dist");
+const buildPath = process.env.NODE_ENV === 'development' ?
+    path.resolve(__dirname, "../client/dist")
+    : path.resolve(__dirname, "../../client/dist");
+console.log("NODE_ENV", process.env.NODE_ENV);
+console.log("buildPath", buildPath);
 const server = express();
 const port = process.env.PORT || 8080;
 
-server.use((req, res, next)=> {
-    const { rawHeaders, httpVersion, method, socket, url } = req;
+server.use((req, res, next) => {
+    const {rawHeaders, httpVersion, method, socket, url} = req;
     console.log(`Mottatt følgende request: ${method} : ${url}`);
     next();
 })
