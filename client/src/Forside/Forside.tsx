c7c4f8fb646fa86cb15d1469419d376d1fc3772a
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-02-03 15:44:15 +0100

    Styling: bruk av Sass med css modules

--- client/src/Forside/Forside.tsx
+++ client/src/Forside/Forside.tsx
@@ -1,14 +1,19 @@
-import {useAltinnOrganisasjoner} from "../hooks/useAltinnOrganisasjoner";
-import {RestStatus} from "../api/rest-status";
-
+import { useAltinnOrganisasjoner } from "../hooks/useAltinnOrganisasjoner";
+import { RestStatus } from "../api/rest-status";
+import styles from "./forside.module.scss";
 
 export const Forside = () => {
+  const altinnOrganisasjoner = useAltinnOrganisasjoner();
 
-    const altinnOrganisasjoner = useAltinnOrganisasjoner();
-
-
-    return (
-        <p>Welcome to Min-IA, Du har tilgang
-            til {altinnOrganisasjoner.status === RestStatus.Suksess ? altinnOrganisasjoner.data?.length : 0}organisasjoner{}</p>
-    )
-}
+  return (
+    <div className={styles.forside}>
+      <p>
+        Welcome to Min-IA, Du har tilgang til{" "}
+        {altinnOrganisasjoner.status === RestStatus.Suksess
+          ? altinnOrganisasjoner.data?.length
+          : 0}
+        organisasjoner{}
+      </p>
+    </div>
+  );
+};
