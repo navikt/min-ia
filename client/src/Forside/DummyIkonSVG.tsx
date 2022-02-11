cfdc0cd3d48419a56614d29881f4046ed5655353
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-02-11 11:08:24 +0100

    Fire første bokser på forsiden (#9)
    * Legge til 4 første bokser på forsiden -- WIP
    
    * Center objekter på siden

---
+++ client/src/Forside/DummyIkonSVG.tsx
@@ -0,0 +1,24 @@
+export const DummyIkonSVG = (props: { className?: string }) => {
+  return (
+    <svg
+      className={props.className}
+      width="80px"
+      height="80px"
+      viewBox="0 0 80 80"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>statistikk-ikon</title>
+      <desc>Created with Sketch.</desc>
+      <g
+        id="statistikk-ikon"
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+      >
+        <circle id="Oval" fill="#CCE1F3" cx="40" cy="40" r="40"></circle>
+      </g>
+    </svg>
+  );
+};
--- client/src/Forside/Forside.tsx
+++ client/src/Forside/Forside.tsx
@@ -1,10 +1,103 @@
 import styles from "./forside.module.scss";
-import { Heading } from "@navikt/ds-react";
+import { LinkPanel } from "@navikt/ds-react";
+import { StatistikkIkonSVG } from "./StatistikkIkonSVG";
+import { SamtalestøtteSVG } from "./SamtalestøtteSVG";
+import { DummyIkonSVG } from "./DummyIkonSVG";
 
 export const Forside = () => {
   return (
     <div className={styles.forside}>
-      <Heading size={"small"}>Welkommen til Forsiden!</Heading>
+      <div className={styles.panelGrid}>
+        <div className={styles.panelRow}>
+          <LinkPanel href="#" className={styles.panel}>
+            <div
+              style={{
+                display: "grid",
+                gridAutoFlow: "row",
+                gap: "var(--navds-spacing-8)",
+                alignItems: "center",
+              }}
+            >
+              <div className={styles.ikonOgTekstWrapper}>
+                <SamtalestøtteSVG className={styles.ikon} />
+                <div>
+                  <LinkPanel.Title>Samtalestøtten</LinkPanel.Title>
+                  <LinkPanel.Description>
+                    Dette verktøyet hjelper deg å strukturere de litt
+                    vanskeligere samtalene med medarbeider.
+                  </LinkPanel.Description>
+                </div>
+              </div>
+            </div>
+          </LinkPanel>
+          <LinkPanel href="#" className={styles.panel}>
+            <div
+              style={{
+                display: "grid",
+                gridAutoFlow: "row",
+                gap: "var(--navds-spacing-8)",
+                alignItems: "center",
+              }}
+            >
+              <div className={styles.ikonOgTekstWrapper}>
+                <StatistikkIkonSVG className={styles.ikon} />
+                <div>
+                  <LinkPanel.Title>Din statistikk</LinkPanel.Title>
+                  <LinkPanel.Description>
+                    Her finner du oversikt over nyttig sykefraværsstatistikk du
+                    kan trenge for å ta gode valg.
+                  </LinkPanel.Description>
+                </div>
+              </div>
+            </div>
+          </LinkPanel>
+        </div>
+        <div className={styles.panelRow}>
+          <LinkPanel href="#" className={styles.panel}>
+            <div
+              style={{
+                display: "grid",
+                gridAutoFlow: "row",
+                gap: "var(--navds-spacing-8)",
+                alignItems: "center",
+              }}
+            >
+              <div className={styles.ikonOgTekstWrapper}>
+                <DummyIkonSVG className={styles.ikon} />
+                <div>
+                  <LinkPanel.Title>Hva gjør de beste</LinkPanel.Title>
+                  <LinkPanel.Description>
+                    Lær av de som forebygger sykefravær på en god, strukturert
+                    måte.
+                  </LinkPanel.Description>
+                </div>
+              </div>
+            </div>
+          </LinkPanel>
+          <LinkPanel href="#" className={styles.panel}>
+            <div
+              style={{
+                display: "grid",
+                gridAutoFlow: "row",
+                gap: "var(--navds-spacing-8)",
+                alignItems: "center",
+              }}
+            >
+              <div className={styles.ikonOgTekstWrapper}>
+                <DummyIkonSVG className={styles.ikon} />
+
+                <div>
+                  <LinkPanel.Title>Kurs og webinarer</LinkPanel.Title>
+                  <LinkPanel.Description>
+                    Her finner du kurs Nav tilbyr til arbeidsgivere som vil bli
+                    bedre i inkluderende arbeidsliv.
+                  </LinkPanel.Description>
+                </div>
+              </div>
+            </div>
+          </LinkPanel>
+        </div>
+      </div>
     </div>
   );
 };
