bba1cf7a59efb8b5e8f698cad7cd7b81aa83f633
runarskagestad <runar.skagestad@nav.no>
2022-02-18 15:14:27 +0100

    Fikset en skrivefeil i en import som knakk kompileringen

--- client/src/LenkeflisEkstern/LenkeflisEkstern.tsx
+++ client/src/LenkeflisEkstern/LenkeflisEkstern.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Lenkeflis-ekstern.module.scss";
+import styles from "./lenkeflis-ekstern.module.scss";
 import { ExternalLink } from "@navikt/ds-icons";
 import { PanelBrødtekstSkjultPåMobil } from "../PanelBrødtekstSkjultPåMobil/PanelBrødtekstSkjultPåMobil";
 
