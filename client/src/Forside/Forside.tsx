ea99c4090dc7d40c929cb7080bdcfa086d78df18
runarskagestad <runar.skagestad@nav.no>
2022-03-03 20:56:25 +0100

    La til innhold i Infographics-komponenten

--- client/src/Forside/Forside.tsx
+++ client/src/Forside/Forside.tsx
@@ -1,73 +1,82 @@
 import styles from "./forside.module.scss";
-import { StatistikkIkonIkon } from "./ikoner/StatistikkIkonIkon";
-import { SamtalestøtteIkon } from "./ikoner/SamtalestøtteIkon";
-import { Lenkeflis } from "../Lenkeflis/Lenkeflis";
-import { HvaGjørDeSomLykkesIkon } from "./ikoner/HvaGjørDeSomLykkesIkon";
-import { KursOgWebinarerIkon } from "./ikoner/KursOgWebinarerIkon";
-import { Calculator } from "@navikt/ds-icons";
-import { LenkeflisEkstern } from "../LenkeflisEkstern/LenkeflisEkstern";
-import { IdebankenIkon } from "./ikoner/IdebankenIkon";
-import { ArbeidsmiljøPortalenIkon } from "./ikoner/ArbeidsmiljøportalenIkon";
+import {StatistikkIkonIkon} from "./ikoner/StatistikkIkonIkon";
+import {SamtalestøtteIkon} from "./ikoner/SamtalestøtteIkon";
+import {Lenkeflis} from "../Lenkeflis/Lenkeflis";
+import {HvaGjørDeSomLykkesIkon} from "./ikoner/HvaGjørDeSomLykkesIkon";
+import {KursOgWebinarerIkon} from "./ikoner/KursOgWebinarerIkon";
+import {Calculator} from "@navikt/ds-icons";
+import {LenkeflisEkstern} from "../LenkeflisEkstern/LenkeflisEkstern";
+import {IdebankenIkon} from "./ikoner/IdebankenIkon";
+import {ArbeidsmiljøPortalenIkon} from "./ikoner/ArbeidsmiljøportalenIkon";
 import React from "react";
-import { Infographic } from "../Infographic/Infographic";
+import {useSykefraværshistorikk} from "../hooks/useSykefraværshistorikk";
+import {RestStatus} from "../integrasjoner/rest-status";
+import {Infographic} from "../Infographic/Infographic";
 
 export const Forside = () => {
-  const bredde = 60;
-  const høyde = 60;
-  return (
-    <div className={styles.forside}>
-      <Infographic />
-      <div className={styles.panelGrid}>
-        <Lenkeflis
-          overskrift={"Samtalestøtten"}
-          ikon={<SamtalestøtteIkon />}
-          brødtekst={
-            "Dette verktøyet hjelper deg å strukturere de litt vanskeligere samtalene med medarbeider."
-          }
-        />
-        <Lenkeflis
-          overskrift={"Din statistikk"}
-          ikon={<StatistikkIkonIkon />}
-          brødtekst={
-            "Her finner du oversikt over nyttig sykefraværsstatistikk du kan trenge for å ta gode valg."
-          }
-        />
-        <Lenkeflis
-          overskrift={"Hva gjør de som lykkes"}
-          ikon={<HvaGjørDeSomLykkesIkon />}
-          brødtekst={
-            "Lær av de som forebygger sykefravær på en god, strukturert måte."
-          }
-        />
-        <Lenkeflis
-          overskrift={"Kurs og webinarer"}
-          ikon={<KursOgWebinarerIkon />}
-          brødtekst={
-            "er finner du kurs Nav tilbyr til arbeidsgivere som vil bli bedre i inkluderende arbeidsliv."
-          }
-        />
-        <Lenkeflis
-          overskrift={"Kalkulator"}
-          ikon={<Calculator />}
-          brødtekst={
-            "er finner du kurs Nav tilbyr til arbeidsgivere som vil bli bedre i inkluderende arbeidsliv."
-          }
-        />
-        <LenkeflisEkstern
-          overskrift={"Idébanken"}
-          ikon={<IdebankenIkon width={bredde} height={høyde} />}
-          brødtekst={
-            "Idébanken har flere nyttige verktøy man kan anvende for å få kontroll over fraværet og arbeidsmiljø."
-          }
-        />
-        <LenkeflisEkstern
-          overskrift={"Arbeidsmiljø&shy;portalen"}
-          ikon={<ArbeidsmiljøPortalenIkon width={bredde} height={høyde} />}
-          brødtekst={
-            "På arbeidsmiljøportalen finner du skreddersydde verktøy for alle bransjer!"
-          }
-        />
-      </div>
-    </div>
-  );
+    const bredde = 60;
+    const høyde = 60;
+    const sykefraværshistorikk = useSykefraværshistorikk();
+
+    const infographicHvisDataOk =
+        sykefraværshistorikk.status !== RestStatus.Suksess ? null : (
+            <Infographic historikk={sykefraværshistorikk.data}/>
+        );
+
+    return (
+        <div className={styles.forside}>
+            {infographicHvisDataOk}
+            <div className={styles.panelGrid}>
+                <Lenkeflis
+                    overskrift={"Samtalestøtten"}
+                    ikon={<SamtalestøtteIkon/>}
+                    brødtekst={
+                        "Dette verktøyet hjelper deg å strukturere de litt vanskeligere samtalene med medarbeider."
+                    }
+                />
+                <Lenkeflis
+                    overskrift={"Din statistikk"}
+                    ikon={<StatistikkIkonIkon/>}
+                    brødtekst={
+                        "Her finner du oversikt over nyttig sykefraværsstatistikk du kan trenge for å ta gode valg."
+                    }
+                />
+                <Lenkeflis
+                    overskrift={"Hva gjør de som lykkes"}
+                    ikon={<HvaGjørDeSomLykkesIkon/>}
+                    brødtekst={
+                        "Lær av de som forebygger sykefravær på en god, strukturert måte."
+                    }
+                />
+                <Lenkeflis
+                    overskrift={"Kurs og webinarer"}
+                    ikon={<KursOgWebinarerIkon/>}
+                    brødtekst={
+                        "er finner du kurs Nav tilbyr til arbeidsgivere som vil bli bedre i inkluderende arbeidsliv."
+                    }
+                />
+                <Lenkeflis
+                    overskrift={"Kalkulator"}
+                    ikon={<Calculator/>}
+                    brødtekst={
+                        "er finner du kurs Nav tilbyr til arbeidsgivere som vil bli bedre i inkluderende arbeidsliv."
+                    }
+                />
+                <LenkeflisEkstern
+                    overskrift={"Idébanken"}
+                    ikon={<IdebankenIkon width={bredde} height={høyde}/>}
+                    brødtekst={
+                        "Idébanken har flere nyttige verktøy man kan anvende for å få kontroll over fraværet og arbeidsmiljø."
+                    }
+                />
+                <LenkeflisEkstern
+                    overskrift={"Arbeidsmiljø&shy;portalen"}
+                    ikon={<ArbeidsmiljøPortalenIkon width={bredde} height={høyde}/>}
+                    brødtekst={
+                        "På arbeidsmiljøportalen finner du skreddersydde verktøy for alle bransjer!"
+                    }
+                />
+            </div>
+        </div>
+    );
 };
--- client/src/Infographic/Infographic.tsx
+++ client/src/Infographic/Infographic.tsx
@@ -1,13 +1,100 @@
-import { FunctionComponent } from "react";
-import { useSykefraværshistorikk } from "../hooks/useSykefraværshistorikk";
-import { RestStatus } from "../integrasjoner/rest-status";
+import {FunctionComponent} from "react";
+import {
+  KvartalsvisSykefraværshistorikk,
+  KvartalsvisSykefraværsprosent,
+  SykefraværshistorikkType,
+} from "../integrasjoner/kvartalsvis-sykefraværshistorikk-api";
 
-export const Infographic: FunctionComponent = () => {
-  const sykefraværshistorikk = useSykefraværshistorikk();
+export const Infographic: FunctionComponent<{
+  historikk: KvartalsvisSykefraværshistorikk[];
+}> = ({historikk}) => {
+  let sisteFraværsprosentNorge = sisteTilgjengelige(
+      sykefraværINorge(historikk)
+  ).prosent;
 
-  if (sykefraværshistorikk.status === RestStatus.Suksess) {
-    return <p>{sykefraværshistorikk.data.length}</p>;
+  let sisteFraværsprosentNæring = sisteTilgjengelige(
+      getSykefraværForNæring(historikk)
+  ).prosent;
+
+  let trend = kalkulerTrend(getSykefraværForNæring(historikk));
+
+  return (
+      <div>
+        <p>{sisteFraværsprosentNorge}</p>
+        <p>{sisteFraværsprosentNæring}</p>
+        <p>Viktigste årsak til sykemelding er: [årsak]</p>
+        <p>{trend}</p>
+      </div>
+  );
+};
+
+// TODO: Refaktorer ut disse funksjonene. Denne komponenten skal ikke være ansvarlig for datatransformasjon.
+// Ideelt skal ikke mer data eksponeres til komponenten enn akkurat hva den har bruk for.
+function sykefraværINorge(
+    fraværshistorikk: KvartalsvisSykefraværshistorikk[]
+): KvartalsvisSykefraværshistorikk {
+  const [historikkForNorge] = fraværshistorikk.filter(
+      (historikk) =>
+          historikk.type === SykefraværshistorikkType.LAND &&
+          historikk.label === "Norge"
+  );
+
+  return historikkForNorge;
+}
+
+function sisteTilgjengelige(historikk: KvartalsvisSykefraværshistorikk) {
+  const sortert = sorterKronologisk(historikk)
+  return sortert[sortert.length - 1]
+}
+
+function getSykefraværForNæring(
+    fraværshistorikk: KvartalsvisSykefraværshistorikk[]
+): KvartalsvisSykefraværshistorikk {
+  const [historikkForNæring] = fraværshistorikk.filter(
+      (historikk) => historikk.type === SykefraværshistorikkType.NÆRING
+  );
+
+  return historikkForNæring;
+}
+
+function getDataForEnkeltkvartal(
+    fraværshistorikk: KvartalsvisSykefraværsprosent[],
+    årstall: number,
+    kvartal: number
+): KvartalsvisSykefraværsprosent {
+  const [enkeltkvartal] = fraværshistorikk.filter(
+      (kvartalstall) =>
+          kvartalstall.årstall === årstall && kvartalstall.kvartal === kvartal
+  );
+
+  return enkeltkvartal;
+}
+
+function kalkulerTrend(
+    fraværsistorikk: KvartalsvisSykefraværshistorikk
+): number | null {
+  const sortert = sorterKronologisk(fraværsistorikk)
+  const nyesteDatapunkt = sortert[sortert.length - 1]
+  const sisteTilgjengeligeÅrstall = nyesteDatapunkt.årstall
+  const sisteTilgjengeligeKvartal = nyesteDatapunkt.kvartal
+  const prosentSisteKvartal = nyesteDatapunkt.prosent
+
+  const prosentSammeKvartalIFjor = getDataForEnkeltkvartal(
+      fraværsistorikk.kvartalsvisSykefraværsprosent,
+      sisteTilgjengeligeÅrstall - 1,
+      sisteTilgjengeligeKvartal
+  )?.prosent;
+
+  if (prosentSisteKvartal && prosentSammeKvartalIFjor) {
+    return prosentSisteKvartal - prosentSammeKvartalIFjor;
   }
 
-  return <p>hellu</p>;
-};
+  return null;
+}
+
+function sorterKronologisk(historikk: KvartalsvisSykefraværshistorikk) {
+  return historikk.kvartalsvisSykefraværsprosent.sort(
+      (e1, e2) => e2.årstall - e1.årstall || e2.kvartal - e1.kvartal
+  );
+
+}
