60e1583cf7df84d059bbc5390b58cd04d2f076fe
Malaz Alkoj <malaz.alkoj@nav.no>
2022-03-04 12:44:58 +0100

    WIP, fikse alignment i inforgraphic

--- client/src/InfographicSnippet/InfographicSnippet.tsx
+++ client/src/InfographicSnippet/InfographicSnippet.tsx
@@ -1,16 +1,19 @@
-import React, {ReactElement, ReactNode} from "react";
+import React, { ReactNode } from "react";
 import styles from "./InfographicSnippet.module.scss";
+import { Label } from "@navikt/ds-react";
 
 export const InfographicSnippet = (props: {
-    ikon: ReactNode;
-    tekst: string;
-    verdi: string
+  ikon: ReactNode;
+  tekst: string;
+  verdi: string;
 }) => {
-    return (
-        <div className={styles.snippetWrapper}>
-            <div className={styles.ikonWrapper}>{props.ikon}</div>
-            {props.tekst}
-            <b>{props.verdi}</b>
-        </div>
-    ); 
+  return (
+    <div className={styles.infographicSnippet}>
+      <div className={styles.ikonWrapper}>{props.ikon}</div>
+      <Label size="small" className={styles.tekst}>
+        {props.tekst}
+        <span style={{ fontWeight: 700 }}>{props.verdi}</span>
+      </Label>
+    </div>
+  );
 };
