c1b57a117eeff98af0e057eaf5eb2f2546b6ee12
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-03-21 13:52:57 +0100

    Bruk 'APP_INGRESS' i stedet av 'APP_URL' for breadcrumbs

--- client/src/pageProps.ts
+++ client/src/pageProps.ts
@@ -1,28 +1,33 @@
-import { DecoratorParts, fetchDecoratorParts } from './utils/dekorator';
+import { DecoratorParts, fetchDecoratorParts } from "./utils/dekorator";
 
 export interface PageProps {
-    appTitle: string;
-    title: string;
-    metaDescription: string;
-    slug: string;
-    decorator: DecoratorParts;
+  appTitle: string;
+  title: string;
+  metaDescription: string;
+  slug: string;
+  decorator: DecoratorParts;
 }
 
-export const getPageProps = async (title: string, slug: string): Promise<PageProps> => {
-    const breadcrumbs = [{ title: encodeURIComponent(title), url: `${process.env.APP_URL}` }];
-    const cacheKey = 'index';
+export const getPageProps = async (
+  title: string,
+  slug: string
+): Promise<PageProps> => {
+  const breadcrumbs = [
+    { title: encodeURIComponent(title), url: `${process.env.APP_INGRESS}` },
+  ];
+  const cacheKey = "index";
 
-    const decorator = await fetchDecoratorParts({
-        siteTitle: title,
-        cacheKey,
-        breadcrumbs,
-    });
+  const decorator = await fetchDecoratorParts({
+    siteTitle: title,
+    cacheKey,
+    breadcrumbs,
+  });
 
-    return {
-        appTitle: title,
-        title,
-        metaDescription: 'Forebygge sykefravær',
-        slug,
-        decorator,
-    };
+  return {
+    appTitle: title,
+    title,
+    metaDescription: "Forebygge sykefravær",
+    slug,
+    decorator,
+  };
 };
