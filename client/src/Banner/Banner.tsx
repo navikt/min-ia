54f67f32fa15c1bfec09f5164c8838a9af664708
Malaz Alkoj <malaz.alkoj@nav.no>
2022-03-23 16:27:21 +0100

    reload sykefraværsstatistikk on orgnr endring

--- client/src/Banner/Banner.tsx
+++ client/src/Banner/Banner.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Bedriftsmeny from "@navikt/bedriftsmeny";
 import "@navikt/bedriftsmeny/lib/bedriftsmeny.css";
 import {
@@ -6,41 +6,52 @@ import {
   RestAltinnOrganisasjoner,
 } from "../integrasjoner/altinnorganisasjon-api";
 import { RestStatus } from "../integrasjoner/rest-status";
-import {createBrowserHistory, createMemoryHistory, History} from "history";
-import LocationState = History.LocationState;
+import { createBrowserHistory, createMemoryHistory, History } from "history";
 import { sendBedriftValgtEvent } from "../amplitude/events";
+import { useRouter } from "next/router";
+import LocationState = History.LocationState;
 
+export interface Organisasjon {
+  Name: string;
+  Type: string;
+  OrganizationNumber: string;
+  OrganizationForm: string;
+  Status: string;
+  ParentOrganizationNumber: any;
+}
 interface Props {
   tittelMedUnderTittel: string | JSX.Element;
   restOrganisasjoner: RestAltinnOrganisasjoner;
 }
 
 const getHistory = () => {
-    if (typeof window === "undefined") return createMemoryHistory();
-    return createBrowserHistory();
-}
+  if (typeof window === "undefined") return createMemoryHistory();
+  return createBrowserHistory();
+};
 
-const Banner: React.FunctionComponent<Props> = (
-  props
-) => {
+const Banner: React.FunctionComponent<Props> = (props) => {
   const { tittelMedUnderTittel, restOrganisasjoner } = props;
   const altinnOrganisasjoner: AltinnOrganisasjon[] =
     restOrganisasjoner.status === RestStatus.Suksess
       ? restOrganisasjoner.data
       : [];
+  const router = useRouter();
 
+  const [history] = useState<History<LocationState>>(getHistory());
+  const onOrganisasjonChange = (organisasjon?: Organisasjon) => {
+    if (organisasjon) {
+      router.push(`?bedrift=${organisasjon.OrganizationNumber}`);
+    }
+    sendBedriftValgtEvent();
+  };
 
-    const [history] = useState<History<LocationState>>(getHistory())
-
-    return (
+  return (
     <div>
       <Bedriftsmeny
         organisasjoner={altinnOrganisasjoner}
         sidetittel={tittelMedUnderTittel}
         history={history}
-        onOrganisasjonChange={() => {
-          sendBedriftValgtEvent();
-        }}
+        onOrganisasjonChange={onOrganisasjonChange}
       />
     </div>
   );
