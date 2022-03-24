1d4f0bcaa4956bfd9ae61cacbb22c5868b8caa70
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-03-24 12:48:28 +0100

    Fiks: feilende test

--- client/src/hooks/useOrgnr.ts
+++ client/src/hooks/useOrgnr.ts
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 
 export const useOrgnr = (): string | undefined => {
   const router = useRouter();
-  const bedrift = router.query.bedrift as string;
+  const bedrift = router?.query.bedrift as string;
 
   if (bedrift === null) {
     return undefined;
