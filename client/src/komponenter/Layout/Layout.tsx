2afe0069e050903735d8fcf3622d1a58c8884e5b
Malaz Alkoj <malaz.alkoj@nav.no>
2022-04-04 13:01:30 +0200

    Fikse tittel for siden

--- client/src/komponenter/Layout/Layout.tsx
+++ client/src/komponenter/Layout/Layout.tsx
@@ -49,7 +49,7 @@ export const Layout = (props: {
     <>
       <Head>
         {headerLinks}
-        <title>props.title</title>
+        <title>{props.title}</title>
         <meta name="description" content={props.description} />
       </Head>
       <main>
