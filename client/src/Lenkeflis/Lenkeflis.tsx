e259396923c076b35a0fff535b9b487ce2e69220
Malaz Alkoj <malaz.alkoj@nav.no>
2022-04-26 10:42:57 +0200

    revertere loader(ikke bruk loader)

--- client/src/Lenkeflis/Lenkeflis.tsx
+++ client/src/Lenkeflis/Lenkeflis.tsx
@@ -1,6 +1,6 @@
-import { LinkPanel, Loader } from "@navikt/ds-react";
+import { LinkPanel } from "@navikt/ds-react";
 import styles from "./Lenkeflis.module.scss";
-import React, { useState } from "react";
+import React from "react";
 import { PanelBrødtekstSkjultPåMobil } from "../PanelBrødtekstSkjultPåMobil/PanelBrødtekstSkjultPåMobil";
 import { sendLenkeKlikketPåEvent } from "../amplitude/events";
 import { useRouter } from "next/router";
@@ -16,14 +16,11 @@ export const Lenkeflis: React.FunctionComponent<{
   const router = useRouter();
   const TIMEOUT_IN_MILLIS = 750;
 
-  const [isLoading, setIsLoading] = useState(false);
-
   const håndtereOnClickMedTimeout = (
     destinasjon: string,
     lenketekst: string,
     timeout: number
   ) => {
-    setIsLoading(true);
     setTimeout(() => {
       router.push(destinasjon);
     }, timeout);
@@ -33,47 +30,37 @@ export const Lenkeflis: React.FunctionComponent<{
   };
 
   return (
-    <div className={styles.loaderOgLinkPanelWrapper}>
-      {isLoading && (
-        <Loader
-          className={styles.loaderRight}
-          variant="interaction"
-          size="medium"
-          title="venter..."
-        />
+    <LinkPanel
+      href={href ? href : "#"}
+      onClickCapture={(e) => {
+        e.preventDefault();
+      }}
+      className={classNames(
+        styles.lenkeflis,
+        fyltoppBakgrunn ? styles.lenkeflis__fyltoppBakgrunn : ""
       )}
-      <LinkPanel
-        href={href ? href : "#"}
-        onClickCapture={(e) => {
-          e.preventDefault();
-        }}
+      onClick={() => {
+        håndtereOnClickMedTimeout(
+          href ? href : "#",
+          overskrift,
+          TIMEOUT_IN_MILLIS
+        );
+      }}
+    >
+      <div
         className={classNames(
-          styles.lenkeflis,
-          fyltoppBakgrunn ? styles.lenkeflis__fyltoppBakgrunn : ""
+          styles.ikonOgTekstWrapper,
+          fyltoppBakgrunn ? styles.ikonOgTekstWrapper__fyltoppBakgrunn : ""
         )}
-        onClick={() => {
-          håndtereOnClickMedTimeout(
-            href ? href : "#",
-            overskrift,
-            TIMEOUT_IN_MILLIS
-          );
-        }}
       >
-        <div
-          className={classNames(
-            styles.ikonOgTekstWrapper,
-            fyltoppBakgrunn ? styles.ikonOgTekstWrapper__fyltoppBakgrunn : ""
-          )}
-        >
-          <div className={styles.ikonWrapper}>{ikon}</div>
-          <div>
-            <LinkPanel.Title>
-              <div dangerouslySetInnerHTML={{ __html: overskrift }} />
-            </LinkPanel.Title>
-            <PanelBrødtekstSkjultPåMobil tekst={brødtekst} />
-          </div>
+        <div className={styles.ikonWrapper}>{ikon}</div>
+        <div>
+          <LinkPanel.Title>
+            <div dangerouslySetInnerHTML={{ __html: overskrift }} />
+          </LinkPanel.Title>
+          <PanelBrødtekstSkjultPåMobil tekst={brødtekst} />
         </div>
-      </LinkPanel>
-    </div>
+      </div>
+    </LinkPanel>
   );
 };
