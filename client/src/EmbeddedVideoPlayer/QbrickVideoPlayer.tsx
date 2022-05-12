dddfc0785262ae9cc275d26654b1a74c4a51f36c
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-05-12 12:23:42 +0200

    Filtrering av videoer nettkurs (#62)
    * Enable pause av en video med injected eventListener i QBrickPlaye
    
    * Sende 'pause' event til alle players når brukeren filtrer listen på nytt
    
    * Deploy til dev
    
    * Loade GoBrain script bare en gang (og ikke i hver player)
    
    * Fjerne multi filtrering, og bruke kun en filter til enhver click
    
    * refaktorering, set mest_Sett som default, fjerne ubrukte attributes i QbrickVideo
    
    * Flytte tittel av video til nettkurs komponent
    
    * Styling av filtreringsknapper
    
    * Fjerne react-helmet
    
    * Rydde opp og slette filer
    
    * Fix styling video + tittel i forskjellige størrelser av video-player
    
    Co-authored-by: erlendaxelsson <erlend.axelsson@gmail.com>
    Co-authored-by: Malaz Alkoj <malaz.alkoj@nav.no>

--- client/src/EmbeddedVideoPlayer/QbrickVideoPlayer.tsx
+++ client/src/EmbeddedVideoPlayer/QbrickVideoPlayer.tsx
@@ -1,40 +1,35 @@
 import styles from "./QbrickVideoPlayer.module.scss";
+import React, { useEffect } from "react";
+import { QbrickVideo } from "../utils/nettkurs-utils";
+import { NavIkon } from "../Nettkurs/ikoner/NavIkon";
 
-interface QbrickVideo {
-  id: string;
-  tags: string[];
-  created: Date;
-  updated: Date;
-  nbOfViews: number; // via API-et?
-  thumbnailUrl: string;
-  metadata: {
-    title: string;
-    description: string;
-  };
-}
-
-interface QbrickVideoPreviewProps {
+export interface QbrickVideoPlayerProps {
   video: QbrickVideo;
 }
 
-// Previews som vises i videolista
-export const QbrickVideoPreview = (props: QbrickVideoPreviewProps) => {
-  return <div>{props.video.metadata.title}</div>;
-};
-
-export interface QbrickVideoPlayerProps {
-  videoId: string;
-}
 export const QbrickVideoPlayer = (props: QbrickVideoPlayerProps) => {
+  useEffect(() => {
+    // @ts-ignore
+    if (window && window.GoBrain) {
+      // @ts-ignore
+      window.GoBrain.widgets(props.video.id).on("play", function () {
+        // @ts-ignore
+        const goBrainWidget = this;
+        document.addEventListener("forcePausePlayer", function (evt) {
+          goBrainWidget.pause();
+        });
+      });
+    }
+  }, [props.video.id]);
+
   const player = () => {
     return {
-      __html: `<div data-gobrain-widgetId="${props.videoId}"
+      __html: `<div data-gobrain-widgetId="${props.video.id}"
         data-gobrain-autoplay="false"  
         data-gobrain-repeat="false" 
         data-gobrain-moduleSettings="{&quot;TopControls&quot;:{&quot;download&quot;:{&quot;enabled&quot;:false},&quot;sharing&quot;:{&quot;enabled&quot;:false}},&quot;MobileControls&quot;:{&quot;download&quot;:{&quot;enabled&quot;:false},&quot;sharing&quot;:{&quot;enabled&quot;:false}}}" 
         data-gobrain-config="https://video.qbrick.com/play2/api/v1/accounts/763558/configurations/wcag2" 
-        data-gobrain-data="https://video.qbrick.com/api/v1/public/accounts/763558/medias/${props.videoId}"></div>
-        <script src="https://play2.qbrick.com/framework/GoBrain.min.js"></script>`,
+        data-gobrain-data="https://video.qbrick.com/api/v1/public/accounts/763558/medias/${props.video.id}"></div>`,
     };
   };
   return (
