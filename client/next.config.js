773109e40f8ff559468455f11df369eefaa1e31b
erlendaxelsson <erlend.axelsson@gmail.com>
2022-07-06 12:17:24 +0200

    Minor change to config to hopefully enable favicon on dev

--- client/next.config.js
+++ client/next.config.js
@@ -27,12 +27,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-            source: "/favicon-32x32.png",
-            destination: "http://localhost:3010/favicon-32x32.png"
+        source: "/favicon-32x32.png",
+        destination: "http://localhost:3010/min-ia/favicon-32x32.png"
       },
       {
-            source: "/favicon-16x16.png",
-            destination: "http://localhost:3010/favicon-16x16.png"
+        source: "/favicon-16x16.png",
+        destination: "http://localhost:3010/min-ia/favicon-16x16.png"
       },
       {
         source: "/qbrick/config/no-preload",
