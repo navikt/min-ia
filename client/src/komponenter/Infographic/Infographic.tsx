9f3d18eb86289ac8bb7bf812a0d4dac3a0649a73
runarskagestad <runar.skagestad@nav.no>
2022-11-21 16:09:06 +0100

    Fikset skjermleser for infographic-flis

--- client/src/komponenter/Infographic/Infographic.tsx
+++ client/src/komponenter/Infographic/Infographic.tsx
@@ -1,17 +1,13 @@
-import { FunctionComponent, ReactNode, useEffect, useState } from "react";
+import {FunctionComponent, ReactNode, useEffect, useState} from "react";
 import styles from "./Infographic.module.scss";
-import { InfographicFlis } from "./InfographicFlis/InfographicFlis";
-import { Bag, HealthCase, NorwegianFlag, Up } from "@navikt/ds-icons";
-import { useOrgnr } from "../../hooks/useOrgnr";
-import { getMiljø } from "../../utils/miljøUtils";
-import {
-  Applikasjon,
-  getUrlForApplikasjon,
-  utledUrlForBedrift,
-} from "../../utils/navigasjon";
-import { InfoModal } from "./InfoModal/InfoModal";
-import { useMobileVersion } from "../../hooks/useMobileVersion";
-import { InngangTilSykefraværsstatistikk } from "./InngangTilSykefraværsstatistikk";
+import {InfographicFlis} from "./InfographicFlis/InfographicFlis";
+import {Bag, HealthCase, NorwegianFlag, Up} from "@navikt/ds-icons";
+import {useOrgnr} from "../../hooks/useOrgnr";
+import {getMiljø} from "../../utils/miljøUtils";
+import {Applikasjon, getUrlForApplikasjon, utledUrlForBedrift,} from "../../utils/navigasjon";
+import {InfoModal} from "./InfoModal/InfoModal";
+import {useMobileVersion} from "../../hooks/useMobileVersion";
+import {InngangTilSykefraværsstatistikk} from "./InngangTilSykefraværsstatistikk";
 
 export interface InfographicData {
   fraværsprosentNorge?: string;
@@ -21,12 +17,10 @@ export interface InfographicData {
   bransjeEllerNæringLabel?: string;
 }
 
-export const Infographic: FunctionComponent<
-  InfographicData & {
-    nedlastingPågår: boolean;
-  }
-> = (props) => {
-  const ikonstorrelse = { width: "50px", height: "50px" };
+export const Infographic: FunctionComponent<InfographicData & {
+  nedlastingPågår: boolean;
+}> = (props) => {
+  const ikonstorrelse = {width: "50px", height: "50px"};
   const orgnr = useOrgnr();
   const miljø = getMiljø();
   const usingMobileVersion = useMobileVersion();
@@ -35,83 +29,83 @@ export const Infographic: FunctionComponent<
 
   useEffect(() => {
     setSykefravarsstatistikkUrl(
-      utledUrlForBedrift(
-        getUrlForApplikasjon(Applikasjon.Sykefraværsstatistikk, miljø),
-        orgnr
-      )
+        utledUrlForBedrift(
+            getUrlForApplikasjon(Applikasjon.Sykefraværsstatistikk, miljø),
+            orgnr
+        )
     );
   }, [orgnr, miljø]);
 
   return (
-    <div className={styles.infographicWrapper}>
-      <InfographicFlis
-        ikon={<NorwegianFlag {...ikonstorrelse} />}
-        innhold={displaytekstSykefraværNorge(props.fraværsprosentNorge)}
-        nedlastingPågår={props.nedlastingPågår}
-      />
-
-      <InfographicFlis
-        ikon={<Bag {...ikonstorrelse} />}
-        innhold={displaytekstSykefraværBransjeEllerNæring(props)}
-        nedlastingPågår={props.nedlastingPågår}
-      />
-
-      <InfographicFlis
-        ikon={<HealthCase {...ikonstorrelse} />}
-        innhold={
-          <>
-            Vanligste årsak til sykemelding i Norge er:{" "}
-            <b>muskel- og skjelettplager</b>
-          </>
-        }
-        nedlastingPågår={props.nedlastingPågår}
-      />
-
-      <InfographicFlis
-        ikon={
-          <Up
-            className={roterTrendpil(
-              props.stigningstallTrendBransjeEllerNæring
-            )}
-            {...ikonstorrelse}
-          />
-        }
-        innhold={displaytekstTrendBransjeEllerNæring(props)}
-        nedlastingPågår={props.nedlastingPågår}
-      />
-
-      <InngangTilSykefraværsstatistikk
-        sykefravarsstatistikkUrl={sykefravarsstatistikkUrl}
-        useMobileVersion={usingMobileVersion}
-      />
-
-      {!usingMobileVersion && (
-        <InfoModal
-          bransjeEllerNæring={props.bransjeEllerNæring}
-          bransjeEllerNæringLabel={props.bransjeEllerNæringLabel}
+      <div className={styles.infographicWrapper}>
+        <InfographicFlis
+            ikon={<NorwegianFlag {...ikonstorrelse} />}
+            innhold={displaytekstSykefraværNorge(props.fraværsprosentNorge)}
+            nedlastingPågår={props.nedlastingPågår}
         />
-      )}
-    </div>
+
+        <InfographicFlis
+            ikon={<Bag {...ikonstorrelse} />}
+            innhold={displaytekstSykefraværBransjeEllerNæring(props)}
+            nedlastingPågår={props.nedlastingPågår}
+        />
+
+        <InfographicFlis
+            ikon={<HealthCase {...ikonstorrelse} />}
+            innhold={
+              <>
+                Vanligste årsak til sykemelding i Norge er:{" "}
+                <b>muskel- og skjelettplager</b>
+              </>
+            }
+            nedlastingPågår={props.nedlastingPågår}
+        />
+
+        <InfographicFlis
+            ikon={
+              <Up
+                  className={roterTrendpil(
+                      props.stigningstallTrendBransjeEllerNæring
+                  )}
+                  {...ikonstorrelse}
+              />
+            }
+            innhold={displaytekstTrendBransjeEllerNæring(props)}
+            nedlastingPågår={props.nedlastingPågår}
+        />
+
+        <InngangTilSykefraværsstatistikk
+            sykefravarsstatistikkUrl={sykefravarsstatistikkUrl}
+            useMobileVersion={usingMobileVersion}
+        />
+
+        {!usingMobileVersion && (
+            <InfoModal
+                bransjeEllerNæring={props.bransjeEllerNæring}
+                bransjeEllerNæringLabel={props.bransjeEllerNæringLabel}
+            />
+        )}
+      </div>
   );
 };
 
 function displaytekstSykefraværNorge(prosent: string | undefined) {
   return (
-    <>
-      Sykefraværet i Norge de siste tolv månedene er: <b>{prosent ?? "- "}%</b>
-    </>
+      <>
+        Sykefraværet i Norge de siste tolv månedene er: <b>{prosent ?? "- "}%</b>
+      </>
   );
 }
 
 const displaytekstSykefraværBransjeEllerNæring = (
-  data: InfographicData
+    data: InfographicData
 ): ReactNode => {
   if (data.fraværsprosentBransjeEllerNæring) {
     return (
-      <>
-        Sykefraværet i din {data.bransjeEllerNæring} de siste tolv månedene er:{" "}
-        <b>{data.fraværsprosentBransjeEllerNæring}%</b>
-      </>
+        <>
+          Sykefraværet i din {data.bransjeEllerNæring} de siste tolv månedene er:{" "}
+          <b>{data.fraværsprosentBransjeEllerNæring}%</b>
+        </>
     );
   } else {
     return `Vi mangler data til beregning av sykefraværet i din ${data.bransjeEllerNæring}`;
@@ -119,13 +113,15 @@ const displaytekstSykefraværBransjeEllerNæring = (
 };
 
 const displaytekstTrendBransjeEllerNæring = (
-  props: InfographicData
+    props: InfographicData
 ): ReactNode => {
   const stigningstall = props.stigningstallTrendBransjeEllerNæring;
+  // Hack for å få skjermleser til å oppføre seg korrekt:
+  const dinBransjeEllerNæring = `din ${props.bransjeEllerNæring}`
   if (isFinite(stigningstall)) {
     return (
       <>
-        Sykefraværet er <b>{stigningstallTilTekst(stigningstall)}</b> i din bransje
+        Sykefraværet er <b>{stigningstallTilTekst(stigningstall)}</b> i {dinBransjeEllerNæring}
       </>
     );
   } else {
