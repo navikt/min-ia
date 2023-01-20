b59fff5c079645793eae55ed5337b9e96036ee0c
erlendaxelsson <erlend.axelsson@gmail.com>
2023-01-20 08:26:12 +0100

    Fjerner unødvendig logging

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,6 @@
 import "../styles/globals.scss";
 import type {AppProps} from "next/app";
-import {useEffect} from "react";
-import {logger} from "../utils/logger";
 
 export default function MyApp({Component, pageProps}: AppProps) {
-
-  //TODO fjern før merge til master
-  useEffect(() => {
-    logger.info("tester frontendlogging fra forebygge fravær")
-  }, [])
-
     return <Component {...pageProps}/>;
 }
