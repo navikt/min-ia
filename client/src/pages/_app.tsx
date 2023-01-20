785af62bf4d5265363b19fdc3e82dc43bfec391b
erlendaxelsson <erlend.axelsson@gmail.com>
2023-01-20 08:13:51 +0100

    Tester logging i dev

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../styles/globals.scss";
 import type {AppProps} from "next/app";
+import {useEffect} from "react";
+import {logger} from "../utils/logger";
 
 export default function MyApp({Component, pageProps}: AppProps) {
+
+  //TODO fjern før merge til master
+  useEffect(() => {
+    logger.info("tester frontendlogging fra forebygge fravær")
+  }, [])
 
     return <Component {...pageProps}/>;
 }
