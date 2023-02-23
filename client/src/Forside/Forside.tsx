a62386f80911751dbc90628adc4f42c78fba2455
Runar Skagestad <runska@gmail.com>
2023-02-23 14:11:39 +0100

    prettify

--- client/src/Forside/Forside.tsx
+++ client/src/Forside/Forside.tsx
@@ -1,106 +1,110 @@
 import styles from "./forside.module.scss";
-import {SamtalestøtteIkon} from "./ikoner/SamtalestøtteIkon";
-import {Calculator, HandsHeart} from "@navikt/ds-icons";
-import {Lenkeflis} from "../Lenkeflis/Lenkeflis";
-import {KursOgWebinarerIkon} from "./ikoner/KursOgWebinarerIkon";
-import {LenkeflisEkstern} from "../LenkeflisEkstern/LenkeflisEkstern";
-import {IdebankenIkon} from "./ikoner/IdebankenIkon";
-import {ArbeidsmiljøPortalenIkon} from "./ikoner/ArbeidsmiljøportalenIkon";
+import { SamtalestøtteIkon } from "./ikoner/SamtalestøtteIkon";
+import { Calculator, HandsHeart } from "@navikt/ds-icons";
+import { Lenkeflis } from "../Lenkeflis/Lenkeflis";
+import { KursOgWebinarerIkon } from "./ikoner/KursOgWebinarerIkon";
+import { LenkeflisEkstern } from "../LenkeflisEkstern/LenkeflisEkstern";
+import { IdebankenIkon } from "./ikoner/IdebankenIkon";
+import { ArbeidsmiljøPortalenIkon } from "./ikoner/ArbeidsmiljøportalenIkon";
 import React from "react";
-import {useAggregertStatistikk} from "../hooks/useAggregertStatistikk";
-import {erFerdigNedlastet, RestStatus} from "../integrasjoner/rest-status";
-import {Infographic} from "../komponenter/Infographic/Infographic";
-import {hentUtInfographicData} from "../komponenter/Infographic/datauthenting";
-import {useOrgnr} from "../hooks/useOrgnr";
-import {Alert} from "@navikt/ds-react";
-import {InkluderendeArbeidslivPanel} from "../InkluderendeArbeidslivPanel/InkluderendeArbeidslivPanel";
-import {tomtDataobjekt} from "../integrasjoner/aggregert-statistikk-api";
-import {leggTilBedriftPåUrl} from "../utils/navigasjon";
+import { useAggregertStatistikk } from "../hooks/useAggregertStatistikk";
+import { erFerdigNedlastet, RestStatus } from "../integrasjoner/rest-status";
+import { Infographic } from "../komponenter/Infographic/Infographic";
+import { hentUtInfographicData } from "../komponenter/Infographic/datauthenting";
+import { useOrgnr } from "../hooks/useOrgnr";
+import { Alert } from "@navikt/ds-react";
+import { InkluderendeArbeidslivPanel } from "../InkluderendeArbeidslivPanel/InkluderendeArbeidslivPanel";
+import { tomtDataobjekt } from "../integrasjoner/aggregert-statistikk-api";
+import { leggTilBedriftPåUrl } from "../utils/navigasjon";
 
 const SAMTALESTØTTE_URL = process.env.NEXT_PUBLIC_SAMTALESTOTTE_URL || "#";
-const FOREBYGGINGSPLAN_URL = process.env.NEXT_PUBLIC_FOREBYGGINGSPLAN_URL || "#"
+const FOREBYGGINGSPLAN_URL =
+  process.env.NEXT_PUBLIC_FOREBYGGINGSPLAN_URL || "#";
 
 export const Forside = () => {
-    const bredde = 60;
-    const høyde = 60;
+  const bredde = 60;
+  const høyde = 60;
 
-    const orgnr = useOrgnr();
+  const orgnr = useOrgnr();
 
-    const samtalestøtteUrlMedOrgnr = leggTilBedriftPåUrl(SAMTALESTØTTE_URL, orgnr)
+  const samtalestøtteUrlMedOrgnr = leggTilBedriftPåUrl(
+    SAMTALESTØTTE_URL,
+    orgnr
+  );
 
-    const aggregertStatistikk = useAggregertStatistikk();
-    const aggregertStatistikkData = erFerdigNedlastet(aggregertStatistikk)
-        ? aggregertStatistikk.data
-        : tomtDataobjekt;
+  const aggregertStatistikk = useAggregertStatistikk();
+  const aggregertStatistikkData = erFerdigNedlastet(aggregertStatistikk)
+    ? aggregertStatistikk.data
+    : tomtDataobjekt;
 
-    const infographicEllerBannerHvisError =
-        aggregertStatistikk.status === RestStatus.Feil ? (
-            <Alert variant={"error"} className={styles.forsideAlert}>
-                Det har skjedd en feil. Vennligst prøv igjen senere.
-            </Alert>
-        ) : (
-            <Infographic
-                {...hentUtInfographicData(aggregertStatistikkData)}
-                nedlastingPågår={!erFerdigNedlastet(aggregertStatistikk)}
-            />
-        );
-
-    return (
-        <>
-            <div className={styles.forside}>
-                {infographicEllerBannerHvisError}
-                <div className={styles.panelGrid}>
-                    <Lenkeflis
-                        overskrift={"Samtalestøtten"}
-                        ikon={<SamtalestøtteIkon/>}
-                        brødtekst={
-                            "Dette verktøyet hjelper deg å strukturere samtaler med medarbeider."
-                        }
-                        href={samtalestøtteUrlMedOrgnr}
-                    />
-                    <Lenkeflis
-                        overskrift={"Video og kurs"}
-                        ikon={<KursOgWebinarerIkon/>}
-                        brødtekst={
-                            "Her finner du videoer og kurs for å forebygge, følge opp og redusere sykefravær."
-                        }
-                        href={"/forebygge-fravar/video-og-kurs"}
-                    />
-                    <Lenkeflis
-                        overskrift={"Fraværs&shy;kalkulator"}
-                        ikon={<Calculator/>}
-                        brødtekst={
-                            "Her får du en rask og enkel oversikt over hvor mye sykefraværet kan koste."
-                        }
-                        href={"/forebygge-fravar/kalkulator"}
-                    />
-                    <Lenkeflis
-                        overskrift={"Forebygg fravær hos dere"}
-                        ikon={<HandsHeart/>}
-                        brødtekst={
-                            "Vi har samlet nyttige forslag til aktiviteter dere kan gjøre for å forebygge fravær."
-                        }
-                        href={FOREBYGGINGSPLAN_URL}
-                    />
-                    <InkluderendeArbeidslivPanel/>
-                    <LenkeflisEkstern
-                        overskrift={"Idébanken"}
-                        ikon={<IdebankenIkon width={bredde} height={høyde}/>}
-                        brødtekst={
-                            "På idébanken finner du ideer, erfaringer og verktøy som kan bidra til bedre arbeidsmiljø og lavere sykefravær."
-                        }
-                        href={"https://www.idebanken.org"}
-                    />
-                    <LenkeflisEkstern
-                        overskrift={"Arbeidsmiljø&shy;portalen"}
-                        ikon={<ArbeidsmiljøPortalenIkon width={bredde} height={høyde}/>}
-                        brødtekst={
-                            "Leter du etter flere gode verktøy for å bedre arbeidsmiljøet? Her finner du kunnskap og digitale verktøy."
-                        }
-                        href={"https://www.arbeidsmiljoportalen.no"}
-                    />
-                </div>
-            </div>
-        </>
+  const infographicEllerBannerHvisError =
+    aggregertStatistikk.status === RestStatus.Feil ? (
+      <Alert variant={"error"} className={styles.forsideAlert}>
+        Det har skjedd en feil. Vennligst prøv igjen senere.
+      </Alert>
+    ) : (
+      <Infographic
+        {...hentUtInfographicData(aggregertStatistikkData)}
+        nedlastingPågår={!erFerdigNedlastet(aggregertStatistikk)}
+      />
     );
+
+  return (
+    <>
+      <div className={styles.forside}>
+        {infographicEllerBannerHvisError}
+        <div className={styles.panelGrid}>
+          <Lenkeflis
+            overskrift={"Samtalestøtten"}
+            ikon={<SamtalestøtteIkon />}
+            brødtekst={
+              "Dette verktøyet hjelper deg å strukturere samtaler med medarbeider."
+            }
+            href={samtalestøtteUrlMedOrgnr}
+          />
+          <Lenkeflis
+            overskrift={"Video og kurs"}
+            ikon={<KursOgWebinarerIkon />}
+            brødtekst={
+              "Her finner du videoer og kurs for å forebygge, følge opp og redusere sykefravær."
+            }
+            href={"/forebygge-fravar/video-og-kurs"}
+          />
+          <Lenkeflis
+            overskrift={"Fraværs&shy;kalkulator"}
+            ikon={<Calculator />}
+            brødtekst={
+              "Her får du en rask og enkel oversikt over hvor mye sykefraværet kan koste."
+            }
+            href={"/forebygge-fravar/kalkulator"}
+          />
+          <Lenkeflis
+            overskrift={"Forebygg fravær hos dere"}
+            ikon={<HandsHeart />}
+            brødtekst={
+              "Vi har samlet nyttige forslag til aktiviteter dere kan gjøre for å forebygge fravær."
+            }
+            href={FOREBYGGINGSPLAN_URL}
+          />
+          <InkluderendeArbeidslivPanel />
+          <LenkeflisEkstern
+            overskrift={"Idébanken"}
+            ikon={<IdebankenIkon width={bredde} height={høyde} />}
+            brødtekst={
+              "På idébanken finner du ideer, erfaringer og verktøy som kan bidra til bedre arbeidsmiljø og lavere sykefravær."
+            }
+            href={"https://www.idebanken.org"}
+          />
+          <LenkeflisEkstern
+            overskrift={"Arbeidsmiljø&shy;portalen"}
+            ikon={<ArbeidsmiljøPortalenIkon width={bredde} height={høyde} />}
+            brødtekst={
+              "Leter du etter flere gode verktøy for å bedre arbeidsmiljøet? Her finner du kunnskap og digitale verktøy."
+            }
+            href={"https://www.arbeidsmiljoportalen.no"}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
--- client/src/komponenter/Infographic/Infographic.tsx
+++ client/src/komponenter/Infographic/Infographic.tsx
@@ -1,129 +1,135 @@
-import {FunctionComponent, ReactNode } from "react";
+import { FunctionComponent, ReactNode } from "react";
 import styles from "./Infographic.module.scss";
-import {InfographicFlis} from "./InfographicFlis/InfographicFlis";
-import {useOrgnr} from "../../hooks/useOrgnr";
-import {useMobileVersion} from "../../hooks/useMobileVersion";
-import {InngangTilSykefraværsstatistikk} from "./InngangTilSykefraværsstatistikk";
-import {BodyLong, Detail, Heading, Label} from "@navikt/ds-react";
-import {leggTilBedriftPåUrl} from "../../utils/navigasjon";
+import { InfographicFlis } from "./InfographicFlis/InfographicFlis";
+import { useOrgnr } from "../../hooks/useOrgnr";
+import { useMobileVersion } from "../../hooks/useMobileVersion";
+import { InngangTilSykefraværsstatistikk } from "./InngangTilSykefraværsstatistikk";
+import { BodyLong, Detail, Heading, Label } from "@navikt/ds-react";
+import { leggTilBedriftPåUrl } from "../../utils/navigasjon";
 
-
-const SYKEFRAVÆRSSTATISTIKK_URL = process.env.NEXT_PUBLIC_SYKEFRAVARSSTATISTIKK_URL || "#";
+const SYKEFRAVÆRSSTATISTIKK_URL =
+  process.env.NEXT_PUBLIC_SYKEFRAVARSSTATISTIKK_URL || "#";
 
 export interface InfographicData {
-    fraværsprosentNorge?: string;
-    fraværsprosentBransjeEllerNæring?: string;
-    stigningstallTrendBransjeEllerNæring: number;
-    bransjeEllerNæring: "bransje" | "næring";
-    bransjeEllerNæringLabel?: string;
+  fraværsprosentNorge?: string;
+  fraværsprosentBransjeEllerNæring?: string;
+  stigningstallTrendBransjeEllerNæring: number;
+  bransjeEllerNæring: "bransje" | "næring";
+  bransjeEllerNæringLabel?: string;
 }
 
 export const Infographic: FunctionComponent<
-    InfographicData & {
+  InfographicData & {
     nedlastingPågår: boolean;
-}
+  }
 > = (props) => {
-    const orgnr = useOrgnr();
-    const usingMobileVersion = useMobileVersion();
-    const sykefraværsstatistikkUrlMedBedrift = leggTilBedriftPåUrl(SYKEFRAVÆRSSTATISTIKK_URL, orgnr)
+  const orgnr = useOrgnr();
+  const usingMobileVersion = useMobileVersion();
+  const sykefraværsstatistikkUrlMedBedrift = leggTilBedriftPåUrl(
+    SYKEFRAVÆRSSTATISTIKK_URL,
+    orgnr
+  );
 
-    return (
-        <div className={styles.infographicWrapper}>
-            <div className={styles.infographicContent__wrapper}>
-                <Heading size={"medium"} level={"2"}>
-                    Sykefraværsstatistikk siste 12 måneder
-                </Heading>
-                <div className={styles.infographicContent}>
-                    <div className={styles.infographicRad}>
-                        <InfographicFlis
-                            innhold={displaytekstSykefraværNorge(props.fraværsprosentNorge)}
-                            nedlastingPågår={props.nedlastingPågår}
-                        />
-
-                        <InfographicFlis
-                            innhold={displaytekstSykefraværBransjeEllerNæring(props)}
-                            nedlastingPågår={props.nedlastingPågår}
-                        />
-                    </div>
-
-                    <div className={styles.infographicRad}>
-                        <InfographicFlis
-                            innhold={
-                                <>
-                                    <BodyLong className={styles.infographicFlisOversikt} size={"small"}>
-                                        Vanligste diagnose i Norge
-                                    </BodyLong>
-                                    <Label style={{textAlign: "center"}}>
-                                        Muskel og skjelett
-                                    </Label>
-                                </>
-                            }
-                            nedlastingPågår={props.nedlastingPågår}
-                        />
-
-                        <InfographicFlis
-                            innhold={displaytekstTrendBransjeEllerNæring(props)}
-                            nedlastingPågår={props.nedlastingPågår}
-                        />
-                    </div>
-                </div>
-            </div>
-            <InngangTilSykefraværsstatistikk
-                sykefravarsstatistikkUrl={sykefraværsstatistikkUrlMedBedrift}
-                useMobileVersion={usingMobileVersion}
+  return (
+    <div className={styles.infographicWrapper}>
+      <div className={styles.infographicContent__wrapper}>
+        <Heading size={"medium"} level={"2"}>
+          Sykefraværsstatistikk siste 12 måneder
+        </Heading>
+        <div className={styles.infographicContent}>
+          <div className={styles.infographicRad}>
+            <InfographicFlis
+              innhold={displaytekstSykefraværNorge(props.fraværsprosentNorge)}
+              nedlastingPågår={props.nedlastingPågår}
             />
+
+            <InfographicFlis
+              innhold={displaytekstSykefraværBransjeEllerNæring(props)}
+              nedlastingPågår={props.nedlastingPågår}
+            />
+          </div>
+
+          <div className={styles.infographicRad}>
+            <InfographicFlis
+              innhold={
+                <>
+                  <BodyLong
+                    className={styles.infographicFlisOversikt}
+                    size={"small"}
+                  >
+                    Vanligste diagnose i Norge
+                  </BodyLong>
+                  <Label style={{ textAlign: "center" }}>
+                    Muskel og skjelett
+                  </Label>
+                </>
+              }
+              nedlastingPågår={props.nedlastingPågår}
+            />
+
+            <InfographicFlis
+              innhold={displaytekstTrendBransjeEllerNæring(props)}
+              nedlastingPågår={props.nedlastingPågår}
+            />
+          </div>
         </div>
-    );
+      </div>
+      <InngangTilSykefraværsstatistikk
+        sykefravarsstatistikkUrl={sykefraværsstatistikkUrlMedBedrift}
+        useMobileVersion={usingMobileVersion}
+      />
+    </div>
+  );
 };
 
 function displaytekstSykefraværNorge(prosent: string | undefined) {
-    return (
-        <>
-            <Detail>I Norge</Detail>
-            <Label>{prosent ?? "- "}%</Label>
-        </>
-    );
+  return (
+    <>
+      <Detail>I Norge</Detail>
+      <Label>{prosent ?? "- "}%</Label>
+    </>
+  );
 }
 
 const displaytekstSykefraværBransjeEllerNæring = (
-    data: InfographicData
+  data: InfographicData
 ): ReactNode => {
-    if (data.fraværsprosentBransjeEllerNæring) {
-        return (
-            <>
-                <Detail>I {data.bransjeEllerNæring}</Detail>
-                <Label>{data.fraværsprosentBransjeEllerNæring}%</Label>
-            </>
-        );
-    } else {
-        return `Vi mangler data til beregning av sykefraværet i din ${data.bransjeEllerNæring}`;
-    }
+  if (data.fraværsprosentBransjeEllerNæring) {
+    return (
+      <>
+        <Detail>I {data.bransjeEllerNæring}</Detail>
+        <Label>{data.fraværsprosentBransjeEllerNæring}%</Label>
+      </>
+    );
+  } else {
+    return `Vi mangler data til beregning av sykefraværet i din ${data.bransjeEllerNæring}`;
+  }
 };
 
 const displaytekstTrendBransjeEllerNæring = (
-    props: InfographicData
+  props: InfographicData
 ): ReactNode => {
-    const stigningstall = props.stigningstallTrendBransjeEllerNæring;
-    // Hack for å få skjermleser til å oppføre seg korrekt:
-    if (isFinite(stigningstall)) {
-        return (
-            <>
-                Trend i bransjen
-                <br/>
-                <b>Fravær {stigningstallTilTekst(stigningstall)}</b>
-            </>
-        );
-    } else {
-        return `Vi mangler data til å kunne beregne utviklingen i sykefraværet i din ${props.bransjeEllerNæring}`;
-    }
+  const stigningstall = props.stigningstallTrendBransjeEllerNæring;
+  // Hack for å få skjermleser til å oppføre seg korrekt:
+  if (isFinite(stigningstall)) {
+    return (
+      <>
+        Trend i bransjen
+        <br />
+        <b>Fravær {stigningstallTilTekst(stigningstall)}</b>
+      </>
+    );
+  } else {
+    return `Vi mangler data til å kunne beregne utviklingen i sykefraværet i din ${props.bransjeEllerNæring}`;
+  }
 };
 
 function stigningstallTilTekst(stigning: number): string {
-    if (stigning > 0) {
-        return "stiger";
-    } else if (stigning < 0) {
-        return "synker";
-    } else {
-        return "er uendret";
-    }
+  if (stigning > 0) {
+    return "stiger";
+  } else if (stigning < 0) {
+    return "synker";
+  } else {
+    return "er uendret";
+  }
 }
