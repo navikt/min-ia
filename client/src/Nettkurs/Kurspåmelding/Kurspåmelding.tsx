b01477f6f0ad48a7b1974c3bac987b0be1196b32
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-03-24 12:03:01 +0100

    Fix kurskalenderikon

--- client/src/Nettkurs/Kurspåmelding/Kurspåmelding.tsx
+++ client/src/Nettkurs/Kurspåmelding/Kurspåmelding.tsx
@@ -1,37 +1,54 @@
 import { FunctionComponent } from "react";
 import { Kurs } from "../../utils/kurs-api";
-import {Lenkepanel, LenkepanelProps} from "../../komponenter/Lenkepanel/Lenkepanel";
-import {KursKalenderIkon} from "../ikoner/KursKalenderIkon";
+import {
+  Lenkepanel,
+  LenkepanelProps,
+} from "../../komponenter/Lenkepanel/Lenkepanel";
+import { KursKalenderIkon } from "../ikoner/KursKalenderIkon";
 import styles from "./kurspåmelding.module.scss";
 
-export interface Kurspåmelding  {
-    tittel: string;
-    undertekst: string;
-    undertekstUtenKurs: string;
-    undertekstUtenKursDel2: string;
-    lenke: LenkepanelProps;
+export interface Kurspåmelding {
+  tittel: string;
+  undertekst: string;
+  undertekstUtenKurs: string;
+  undertekstUtenKursDel2: string;
+  lenke: LenkepanelProps;
 }
-interface Props{
-kurspåmelding:Kurspåmelding|null;
-nesteNettkurs:Kurs| undefined;
+
+interface Props {
+  kurspåmelding: Kurspåmelding | null;
+  nesteNettkurs: Kurs | undefined;
 }
-export const Kurspåmelding: FunctionComponent<Props> = ({ kurspåmelding,nesteNettkurs }: Props) => {
-  return <>{
-      nesteNettkurs !== undefined && kurspåmelding !== null
-        ? <>
-            {/*<div>{kurspåmelding.tittel}</div>*/}
-            <div className={styles.kurspåmelding_tekst}>
-                {kurspåmelding.undertekst}</div>
-            <Lenkepanel
-                tittel={kurspåmelding.lenke.tittel}
-                ikon={KursKalenderIkon}
-                href={kurspåmelding.lenke.href}
-                className={styles.kurspåmelding__folger}
-            />
-          </>
-        : <>
-            <div className={styles.kurspåmelding_tekst}>{kurspåmelding?.undertekstUtenKurs}</div>
-            <div className={styles.kurspåmelding__folger}>{kurspåmelding?.undertekstUtenKursDel2}</div>
-          </>
-  }</>;
+
+export const Kurspåmelding: FunctionComponent<Props> = ({
+  kurspåmelding,
+  nesteNettkurs,
+}: Props) => {
+  return (
+    <>
+      {nesteNettkurs !== undefined && kurspåmelding !== null ? (
+        <>
+          {/*<div>{kurspåmelding.tittel}</div>*/}
+          <div className={styles.kurspåmelding_tekst}>
+            {kurspåmelding.undertekst}
+          </div>
+          <Lenkepanel
+            tittel={kurspåmelding.lenke.tittel}
+            ikon={<KursKalenderIkon />}
+            href={kurspåmelding.lenke.href}
+            className={styles.kurspåmelding__folger}
+          />
+        </>
+      ) : (
+        <>
+          <div className={styles.kurspåmelding_tekst}>
+            {kurspåmelding?.undertekstUtenKurs}
+          </div>
+          <div className={styles.kurspåmelding__folger}>
+            {kurspåmelding?.undertekstUtenKursDel2}
+          </div>
+        </>
+      )}
+    </>
+  );
 };
