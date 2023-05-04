00712bcefba211908817669a5725d7ad86665b6f
Runar Skagestad <runska@gmail.com>
2023-05-04 16:46:56 +0200

    Legger APP_BASE_PATH til redirect-endepunkt

--- server/src/authentication.ts
+++ server/src/authentication.ts
@@ -3,7 +3,7 @@ import { APP_BASE_PATH } from "./config/meta.js";
 
 export const setupLoginRedirect = (server: Express) => {
   server.get(`${APP_BASE_PATH}/redirect-til-login`, (request, response) => {
-    const wonderwallLoginEndpoint = `/oauth2/login?redirect=${
+    const wonderwallLoginEndpoint = `${APP_BASE_PATH}/oauth2/login?redirect=${
       request.query.redirect as string
     }`;
     response.redirect(wonderwallLoginEndpoint);
