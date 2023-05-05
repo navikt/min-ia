4f19d098d023f5239bb4b90dc64a691fefce2e9d
Runar Skagestad <runar.skagestad@nav.no>
2023-05-05 09:42:18 +0200

    Blir kvitt overkomplisert login-logikk, og fikser login-redirect fra dekoratøren (#126)
    * Blir kvitt overkomplisert login-logikk
    
    * Legger til redirect i dekoratoren

--- server/src/login/common.ts
+++
@@ -1,6 +0,0 @@
-import {APP_BASE_PATH} from "../config/meta.js";
-
-export const getLoginTilOauth2 = (redirectUrl: string): string => {
-    const referrerUrl = `${process.env.APP_INGRESS}/success?redirect=${redirectUrl}`;
-    return `${APP_BASE_PATH}/oauth2/login?redirect=${referrerUrl}`;
-};
--- server/src/server.ts
+++ server/src/server.ts
@@ -9,28 +9,42 @@ import { requestRateLimiter } from "./config/middleware/requestRateLimiter.js";
 import { SERVER_PORT } from "./config/meta.js";
 import { prometheus } from "./config/middleware/prometheus.js";
 import { isAlive, isReady } from "./healthcheck.js";
-import { setupApiRoutes } from "./routes.js";
 import { isMockApp } from "./util/environment";
+import {
+  applyNotifikasjonMiddleware,
+  setupBackendApiProxy, setupIaTjenestermetrikkerProxy,
+  setupKursoversiktApiProxy,
+  setupNotifikasjonBrukerAPIProxyMock
+} from "./config/middleware/proxyMiddleware";
+import {backendApiProxyMock} from "./local/proxyMiddlewareMock";
+import {setupWonderwallLoginRedirect} from "./authentication";
 
 const initServer = async () => {
   logger.info(
     "Starting server (server.ts) on Node environment " + process.env.NODE_ENV
   );
   const server = express();
-  isAlive(server);
 
+  isAlive(server);
   server.use(correlationIdMiddleware);
   server.use(requestLoggingMiddleware);
   server.use(prometheus);
   server.use(requestRateLimiter);
   server.use(cookieParser()); // Bruker vi cookieParseren lenger?
 
-  if (!isMockApp()) {
+  if (isMockApp()) {
+    setupNotifikasjonBrukerAPIProxyMock(server);
+    backendApiProxyMock(server);
+  } else {
     await initIdporten();
     await initTokenX();
+    setupWonderwallLoginRedirect(server);
+    setupBackendApiProxy(server);
+    setupKursoversiktApiProxy(server);
+    setupIaTjenestermetrikkerProxy(server);
+    applyNotifikasjonMiddleware(server);
   }
 
-  setupApiRoutes(server);
   server.listen(SERVER_PORT, () => {
     logger.info("Server listening on port " + SERVER_PORT);
   });
