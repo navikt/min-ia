1554495e3b1a53ffafb65643cefb70aea5302bb0
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-06-08 14:58:55 +0200

    Skjul ikoner fra skjermleser

--- client/src/Forside/AndreForebyggendeVerktøy/AndreForebyggendeVerktoy.tsx
+++ client/src/Forside/AndreForebyggendeVerktøy/AndreForebyggendeVerktoy.tsx
@@ -1,28 +1,32 @@
 import styles from "./andreForebyggendeVerktøy.module.scss";
-import {Lenkeflis} from "../../Lenkeflis/Lenkeflis";
-import {Calculator, SpeechBubble, VideoRoom} from "@navikt/ds-icons";
+import { Lenkeflis } from "../../Lenkeflis/Lenkeflis";
+import { Calculator, SpeechBubble, VideoRoom } from "@navikt/ds-icons";
 import React from "react";
-import {Heading} from "@navikt/ds-react";
+import { Heading } from "@navikt/ds-react";
 
 export function AndreForebyggendeVerktoy(props: { href: string }) {
-    return <div className={styles.andreForebyggendeVerktøy}>
-        <Heading size={"large"} level={"2"}>Andre forebyggende verktøy</Heading>
-        <div className={styles.panelGrid}>
-            <Lenkeflis
-                overskrift={"Samtalestøtten"}
-                ikon={<SpeechBubble/>}
-                href={props.href}
-            />
-            <Lenkeflis
-                overskrift={"Video og kurs"}
-                ikon={<VideoRoom/>}
-                href={"/forebygge-fravar/video-og-kurs"}
-            />
-            <Lenkeflis
-                overskrift={"Fraværskalkulator"}
-                ikon={<Calculator/>}
-                href={"/forebygge-fravar/kalkulator"}
-            />
-        </div>
-    </div>;
+  return (
+    <div className={styles.andreForebyggendeVerktøy}>
+      <Heading size={"large"} level={"2"}>
+        Andre forebyggende verktøy
+      </Heading>
+      <div className={styles.panelGrid}>
+        <Lenkeflis
+          overskrift={"Samtalestøtten"}
+          ikon={<SpeechBubble aria-hidden />}
+          href={props.href}
+        />
+        <Lenkeflis
+          overskrift={"Video og kurs"}
+          ikon={<VideoRoom aria-hidden />}
+          href={"/forebygge-fravar/video-og-kurs"}
+        />
+        <Lenkeflis
+          overskrift={"Fraværskalkulator"}
+          ikon={<Calculator aria-hidden />}
+          href={"/forebygge-fravar/kalkulator"}
+        />
+      </div>
+    </div>
+  );
 }
