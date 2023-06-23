a64cc613377807210fce10851fbfcf1f2d472a4a
Tharald Stray <tharald.jorgen.bachke.stray@nav.no>
2023-06-23 15:55:59 +0200

    mock useOrgnr i lenkeflis-test

--- client/src/Lenkeflis/Lenkeflis.test.tsx
+++ client/src/Lenkeflis/Lenkeflis.test.tsx
@@ -1,37 +1,36 @@
-import {render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import {Lenkeflis} from "./Lenkeflis";
+import { Lenkeflis } from "./Lenkeflis";
 import logEvent from "../amplitude/logEvent";
 
 jest.mock("../amplitude/logEvent");
+jest.mock("../hooks/useOrgnr", () => ({
+  useOrgnr: () => "999999999",
+}));
 
 beforeEach(() => {
-    // window.location-assign er ikke implementert i jest, så vi må mocke den
-    // @ts-ignore
-    delete window.location;
-    // @ts-ignore
-    window.location = {assign: jest.fn()};
+  // window.location-assign er ikke implementert i jest, så vi må mocke den
+  // @ts-ignore
+  delete window.location;
+  // @ts-ignore
+  window.location = { assign: jest.fn() };
 });
 
 it("sender navigere-event ved klikk på en lenkeflis", async () => {
-    render(
-        <Lenkeflis
-            overskrift="Link"
-            brødtekst="Klikk her"
-            href="destinasjon"
-        />
-    );
+  render(
+    <Lenkeflis overskrift="Link" brødtekst="Klikk her" href="destinasjon" />
+  );
 
-    const statistikklenke = screen.getByRole("link", {
-        name: "Link Klikk her",
-    });
+  const statistikklenke = screen.getByRole("link", {
+    name: "Link Klikk her",
+  });
 
-    const user = userEvent.setup();
-    await user.click(statistikklenke);
+  const user = userEvent.setup();
+  await user.click(statistikklenke);
 
-    expect(logEvent).toBeCalledTimes(1);
-    expect(logEvent).toHaveBeenCalledWith("navigere", {
-        destinasjon: "destinasjon",
-        lenketekst: "Link",
-    });
+  expect(logEvent).toBeCalledTimes(1);
+  expect(logEvent).toHaveBeenCalledWith("navigere", {
+    destinasjon: "destinasjon",
+    lenketekst: "Link",
+  });
 });
