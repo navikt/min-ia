1708ff0067c41e1721852c56586295dfb401d9d0
Tharald Stray <tharald.jorgen.bachke.stray@nav.no>
2023-06-26 14:55:13 +0200

    accept post requests for notification graphql requests, remove debug logging

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,34 +1,15 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { randomUUID } from "crypto";
-import { logger } from "./utils/logger";
-import { isMockApp } from "./utils/envUtils";
-import { exchangeIdportenSubjectToken } from "@navikt/tokenx-middleware";
 
 export const runtime = "nodejs";
 
 export async function middleware(request: NextRequest) {
-  console.log("HELLO");
-  console.log(request.nextUrl.pathname);
-
   if (noCorrelationIdHeaderExist(request)) {
     addCorrelationIdHeader(request);
   }
 
-  if (isMockApp()) {
-    console.log("mock app");
-  } else {
-    console.log("real app");
-    console.log(request.nextUrl.pathname.includes("/authenticated/"));
-  }
-
-  if (
-    request.nextUrl.pathname.startsWith(
-      "/api/authenticated/sykefravarsstatistikk"
-    )
-  ) {
-    console.log("sykefravarsstatistikk");
-  }
+  return NextResponse.next();
 }
 
 export const config = {
--- client/src/pages/api/authenticated/notifikasjoner.ts
+++ client/src/pages/api/authenticated/notifikasjoner.ts
@@ -7,7 +7,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "GET")
+  if (req.method !== "POST")
     return res.status(405).json({ error: "Method Not Allowed" });
 
   if (process.env.SYKEFRAVARSSTATISTIKK_API_AUDIENCE === undefined) {
