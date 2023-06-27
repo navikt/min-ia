5e59d037f5e4268e78a539d5d461c35ef4c91a61
Tharald Stray <tharald.jorgen.bachke.stray@nav.no>
2023-06-27 14:45:14 +0200

    disable token exchange in notification request, update api audience

--- client/src/pages/api/authenticated/notifikasjoner.ts
+++ client/src/pages/api/authenticated/notifikasjoner.ts
@@ -10,25 +10,26 @@ export default async function handler(
   if (req.method !== "POST")
     return res.status(405).json({ error: "Method Not Allowed" });
 
-  if (process.env.SYKEFRAVARSSTATISTIKK_API_AUDIENCE === undefined) {
-    logger.error("SYKEFRAVARSSTATISTIKK_API_AUDIENCE not set");
+  /*
+  if (process.env.NOTIFIKASJON_API_AUDIENCE === undefined) {
+    logger.error("NOTIFIKASJON_API_AUDIENCE not set");
     return res.status(500).json({ error: "authentication failed" });
   }
 
   const newAuthToken = await exchangeIdportenSubjectToken(
     req,
-    process.env.SYKEFRAVARSSTATISTIKK_API_AUDIENCE
+    process.env.NOTIFIKASJON_API_AUDIENCE
   );
   if (!newAuthToken) {
     return res.status(400).json({ error: "authentication failed" });
   }
+  */
 
   await proxyApiRouteRequest({
     req,
     res,
     hostname: "notifikasjon-bruker-api.fager.svc.cluster.local",
     path: "/api/graphql",
-    bearerToken: newAuthToken,
     // use https: false if you are going through service discovery
     https: false,
   });
