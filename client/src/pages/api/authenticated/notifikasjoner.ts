0d2b41dfbd351cefb3136c4f03dafd03eb6a9939
Tharald Stray <tharald.jorgen.bachke.stray@nav.no>
2023-06-27 15:23:15 +0200

    enable token exchange for notification request, add debug logging

--- client/src/pages/api/authenticated/notifikasjoner.ts
+++ client/src/pages/api/authenticated/notifikasjoner.ts
@@ -7,10 +7,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log("hei fra proxy til notifikasjoner");
+  console.log(JSON.stringify(req.headers));
+  console.log(JSON.stringify(req.cookies));
   if (req.method !== "POST")
     return res.status(405).json({ error: "Method Not Allowed" });
 
-  /*
   if (process.env.NOTIFIKASJON_API_AUDIENCE === undefined) {
     logger.error("NOTIFIKASJON_API_AUDIENCE not set");
     return res.status(500).json({ error: "authentication failed" });
@@ -23,7 +25,6 @@ export default async function handler(
   if (!newAuthToken) {
     return res.status(400).json({ error: "authentication failed" });
   }
-  */
 
   await proxyApiRouteRequest({
     req,
--- client/src/pages/api/authenticated/sykefravarsstatistikk/organisasjoner.ts
+++ client/src/pages/api/authenticated/sykefravarsstatistikk/organisasjoner.ts
@@ -24,7 +24,6 @@ export default async function handler(
     return res.status(401).json({ error: "authentication failed" });
   }
 
-  console.log("hei fra proxy til organisasjoner");
   await proxyApiRouteRequest({
     req,
     res,
