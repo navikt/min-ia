3df51c130ae4fde376769c391fa746596ea57d1d
Tharald Stray <tharald.jorgen.bachke.stray@nav.no>
2023-07-13 11:44:19 +0200

    fix metrics url

--- client/src/utils/konstanter.ts
+++ client/src/utils/konstanter.ts
@@ -3,5 +3,5 @@ export const API_BASE_PATH = BASE_PATH + "/api";
 export const AUTHENTICATED_BASE_PATH = API_BASE_PATH + "/authenticated";
 export const SYKEFRAVARSSTATISTIKK_BASE_PATH =
   AUTHENTICATED_BASE_PATH + "/sykefravarsstatistikk";
-export const METRIKKER_URL = API_BASE_PATH + "/metrikker";
+export const METRIKKER_URL = AUTHENTICATED_BASE_PATH + "/metrikker";
 export const SCREEN_MOBILE_MIN = 480;
