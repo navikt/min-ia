308f9c488eb03e0debf170b2ba7f7019795f46e8
Tharald Stray <tharald.jorgen.bachke.stray@nav.no>
2023-07-13 18:12:50 +0200

    delete server

--- server/src/config/meta.ts
+++
@@ -1,2 +0,0 @@
-export const APP_BASE_PATH = "/forebygge-fravar";
-export const SERVER_PORT = "3010";
--- server/src/config/middleware/prometheus.ts
+++
@@ -1,7 +0,0 @@
-import promBundle from "express-prom-bundle";
-import { APP_BASE_PATH } from "../meta.js";
-
-export const prometheus = promBundle({
-  includePath: true,
-  metricsPath: `${APP_BASE_PATH}/internal/metrics`,
-});
