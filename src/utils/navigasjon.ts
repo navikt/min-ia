00da48d6b96a88f16b60d4f00b52280995f01ad4
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-07-24 10:37:59 +0200

    Ga metrikker 10 ganger så lang tid på å sende før vi gir opp.

--- src/utils/navigasjon.ts
+++ src/utils/navigasjon.ts
@@ -4,7 +4,7 @@
 export const navigerEtterCallbacks = async (
   destinasjon: string,
   callbacks: (() => Promise<boolean>)[],
-  maksVentetid = 1000
+  maksVentetid = 10000
 ) => {
   setTimeout(() => {
     window.location.href = destinasjon;
