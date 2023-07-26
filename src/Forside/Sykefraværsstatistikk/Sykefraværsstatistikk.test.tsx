a8a53355c7cc163af9fa255ec868745add6fbaf1
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-07-26 13:39:39 +0200

    Fjernet ubrukt import

--- src/Forside/Sykefraværsstatistikk/Sykefraværsstatistikk.test.tsx
+++ src/Forside/Sykefraværsstatistikk/Sykefraværsstatistikk.test.tsx
@@ -1,8 +1,4 @@
-import {
-  render,
-  screen,
-  waitForElementToBeRemoved,
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import {
   AggregertStatistikkDto,
