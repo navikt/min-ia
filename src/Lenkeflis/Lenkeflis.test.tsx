af989abdb6aa8f41cf3ad63f1a7f28ca940824bf
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-08-04 13:30:39 +0200

    La til tester for metrikkutsendelse på Kalkulator og Lenkeflis.

--- src/Lenkeflis/Lenkeflis.test.tsx
+++ src/Lenkeflis/Lenkeflis.test.tsx
@@ -2,21 +2,32 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Lenkeflis } from "./Lenkeflis";
 import logEvent from "../amplitude/logEvent";
-import { axe } from 'jest-axe';
+import { axe } from "jest-axe";
+import { sendLevertInnloggetIaTjeneste } from "../integrasjoner/ia-tjenestemetrikker-api";
 
 jest.mock("../amplitude/logEvent");
 jest.mock("../hooks/useOrgnr", () => ({
   useOrgnr: () => "999999999",
 }));
+jest.mock("../integrasjoner/ia-tjenestemetrikker-api", () => ({
+  __esModule: true,
+  ...jest.requireActual("../integrasjoner/ia-tjenestemetrikker-api"),
+  sendLevertInnloggetIaTjeneste: jest.fn(),
+}));
 
 beforeEach(() => {
   // window.location-assign er ikke implementert i jest, så vi må mocke den
   // eslint-disable-next-line
-    // @ts-ignore
+  // @ts-ignore
   delete window.location;
-    // eslint-disable-next-line
+  // eslint-disable-next-line
   // @ts-ignore
   window.location = { assign: jest.fn() };
+});
+
+afterEach(() => {
+  jest.resetAllMocks();
+  jest.fn().mockClear();
 });
 
 it("sender navigere-event ved klikk på en lenkeflis", async () => {
@@ -38,10 +49,27 @@ it("sender navigere-event ved klikk på en lenkeflis", async () => {
   });
 });
 
-test("uu-feil fra axe", async () => {
-    const { container: myContainer } = render(
-      <Lenkeflis overskrift="Link" brødtekst="Klikk her" href="destinasjon" />
-    );
-    const results = await axe(myContainer);
-    expect(results).toHaveNoViolations();
+it("Kaller sendLevertInnloggetIaTjeneste ved klikk på en lenkeflis", async () => {
+  render(
+    <Lenkeflis overskrift="Link" brødtekst="Klikk her" href="destinasjon" />
+  );
+  const user = userEvent.setup();
+
+  const statistikklenke = screen.getByRole("link", {
+    name: "Link Klikk her",
   });
+
+  expect(sendLevertInnloggetIaTjeneste).toHaveBeenCalledTimes(0);
+
+  await user.click(statistikklenke);
+
+  expect(sendLevertInnloggetIaTjeneste).toHaveBeenCalledTimes(1);
+});
+
+test("uu-feil fra axe", async () => {
+  const { container: myContainer } = render(
+    <Lenkeflis overskrift="Link" brødtekst="Klikk her" href="destinasjon" />
+  );
+  const results = await axe(myContainer);
+  expect(results).toHaveNoViolations();
+});
