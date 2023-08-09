49388e2c8f5c319c1d82a6fc5c9c4ffa1bcc6ee8
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-08-09 15:21:01 +0200

    La til test for metrikkutsendelse fra videospiller (selve spilleren måtte mockes).

--- src/VideoOgKurs/video-og-kursTest.test.tsx
+++ src/VideoOgKurs/video-og-kursTest.test.tsx
@@ -1,60 +1,100 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import VideoOgKurs from "../pages/video-og-kurs";
 import logEvent from "../amplitude/logEvent";
-import { useRouter } from "next/router";
+import { QbrickVideoPlayer } from "../EmbeddedVideoPlayer/QbrickVideoPlayer";
+import { sendLevertInnloggetIaTjeneste } from "../integrasjoner/ia-tjenestemetrikker-api";
 
 jest.mock("../../src/amplitude/logEvent");
-afterEach(() => {
-  jest.resetAllMocks();
-  jest.fn().mockClear();
-});
-
-beforeEach(() => {
-  // window.location-assign er ikke implementert i jest, så vi må mocke den
-  // eslint-disable-next-line
-  // @ts-ignore
-  delete window.location;
-  // eslint-disable-next-line
-  // @ts-ignore
-  delete window.postMessage;
-  // eslint-disable-next-line
-  // @ts-ignore
-  window.location = { assign: jest.fn() };
-  // eslint-disable-next-line
-  // @ts-ignore
-  window.postMessage = jest.fn();
-});
-
-jest.mock("next/router", () => ({
-  useRouter: jest.fn(),
+jest.mock("../../src/EmbeddedVideoPlayer/QbrickVideoPlayer");
+jest.mock("../../src/integrasjoner/ia-tjenestemetrikker-api", () => ({
+  ...jest.requireActual("../../src/integrasjoner/ia-tjenestemetrikker-api"),
+  sendLevertInnloggetIaTjeneste: jest.fn(() => Promise.resolve(true)),
+}));
+jest.mock("../../src/hooks/useOrgnr", () => ({
+  useOrgnr: () => "999999999",
 }));
 
-// eslint-disable-next-line
-// @ts-ignore
-useRouter.mockReturnValue({
-  query: {},
-  push: jest.fn(),
-});
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(() => ({
+    query: {},
+    push: jest.fn(),
+  })),
+}));
 
-it("sender nettkurs-filter-valgt-event ved klikk på Psykisk helse-filter", async () => {
+describe("VideoOgKurs", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.fn().mockClear();
+    document.addEventListener(
+      // fjern/stopp gamle eventlisteners.
+      "videoAvspilles",
+      (e) => {
+        e.stopImmediatePropagation();
+        e.stopPropagation();
+      },
+      true
+    );
+  });
+
   const user = userEvent.setup();
-  renderNettkurs();
 
-  const knappetekst = "Psykisk helse";
-  const psykiskHelseFilter = screen.getByRole("button", {
-    name: knappetekst,
+  it("sender nettkurs-filter-valgt-event ved klikk på Psykisk helse-filter", async () => {
+    renderNettkurs();
+
+    const knappetekst = "Psykisk helse";
+    const psykiskHelseFilter = screen.getByRole("button", {
+      name: knappetekst,
+    });
+    await user.click(psykiskHelseFilter);
+
+    expect(logEvent).toHaveBeenCalledTimes(1);
+    expect(logEvent).toHaveBeenCalledWith("chip valgt", {
+      chipId: knappetekst,
+      tekst: knappetekst,
+    });
   });
-  await user.click(psykiskHelseFilter);
 
-  expect(logEvent).toHaveBeenCalledTimes(1);
-  expect(logEvent).toHaveBeenCalledWith("chip valgt", {
-    chipId: knappetekst,
-    tekst: knappetekst,
+  it("sender metrikker etter play-klikk", async () => {
+    (QbrickVideoPlayer as jest.Mock).mockImplementation(MockQbrickVideoPlayer);
+    renderNettkurs();
+
+    expect(sendLevertInnloggetIaTjeneste).not.toHaveBeenCalled();
+
+    const playknapper = screen.getAllByText("mock qbrick play button");
+    await user.click(playknapper[0]);
+
+    await waitFor(() => {
+      expect(sendLevertInnloggetIaTjeneste).toHaveBeenCalledTimes(1);
+    });
+
+    // sjekk at det ikke sendes flere metrikker ved klikk forskjellige videoer
+    await user.click(playknapper[1]);
+    await waitFor(() => {
+      expect(sendLevertInnloggetIaTjeneste).toHaveBeenCalledTimes(1);
+    });
   });
 });
 
-const renderNettkurs = () => {
+function MockQbrickVideoPlayer() {
+  return (
+    <div>
+      <span>
+        This is a dummy component, and should never be seen outside a code
+        editor or terminal
+      </span>
+      <button
+        onClick={() =>
+          document.dispatchEvent(new CustomEvent("videoAvspilles"))
+        }
+      >
+        mock qbrick play button
+      </button>
+    </div>
+  );
+}
+
+const renderNettkurs = () =>
   render(
     <VideoOgKurs
       page={{
@@ -66,4 +106,3 @@ const renderNettkurs = () => {
       grafanaAgentUrl=""
     />
   );
-};
