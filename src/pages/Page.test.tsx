5575be9d63bf44d517f559ccca3fa7d16384197d
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-08-11 11:48:32 +0200

    La til samlede tester for metrikker over hele siden.

---
+++ src/pages/Page.test.tsx
@@ -0,0 +1,167 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { sendLevertInnloggetIaTjeneste } from "../integrasjoner/ia-tjenestemetrikker-api";
+import VideoOgKurs from "../pages/video-og-kurs";
+import { QbrickVideoPlayer } from "../EmbeddedVideoPlayer/QbrickVideoPlayer";
+import { Fraværskalulator } from "../komponenter/Kalkulator/Kalkulator";
+import Home from ".";
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null),
+    };
+  },
+}));
+jest.mock("../integrasjoner/ia-tjenestemetrikker-api", () => ({
+  __esModule: true,
+  ...jest.requireActual("../integrasjoner/ia-tjenestemetrikker-api"),
+  sendLevertInnloggetIaTjeneste: jest.fn(),
+}));
+jest.mock("../hooks/useOrgnr", () => ({
+  useOrgnr: () => "999999999",
+}));
+jest.mock("../amplitude/logEvent");
+jest.mock("../EmbeddedVideoPlayer/QbrickVideoPlayer");
+const user = userEvent.setup();
+
+describe("Metrikktester av hele siden", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+    jest.fn().mockClear();
+  });
+  describe("Kalkulator", () => {
+    it("Kaller sendLevertInnloggetIaTjeneste ved endring av modus", async () => {
+      render(
+        <Fraværskalulator
+          fraværsprosentVirksomhet="14,0"
+          tapteDagsverk="7800"
+          muligeDagsverk="52000"
+          nedlastingPågår={false}
+        />
+      );
+
+      const dagsverkLenke = screen.getByText("Dagsverk");
+
+      expect(sendLevertInnloggetIaTjeneste).toHaveBeenCalledTimes(0);
+
+      await user.click(dagsverkLenke);
+
+      expect(sendLevertInnloggetIaTjeneste).toHaveBeenCalledTimes(1);
+    });
+  });
+  describe("Video og kurs", () => {
+    afterEach(() => {
+      document.addEventListener(
+        // fjern/stopp gamle eventlisteners.
+        "videoAvspilles",
+        (e) => {
+          e.stopImmediatePropagation();
+          e.stopPropagation();
+        },
+        true
+      );
+    });
+
+    it("sender metrikker etter play-klikk", async () => {
+      (QbrickVideoPlayer as jest.Mock).mockImplementation(
+        MockQbrickVideoPlayer
+      );
+      renderNettkurs();
+
+      expect(sendLevertInnloggetIaTjeneste).not.toHaveBeenCalled();
+
+      const playknapper = screen.getAllByText("mock qbrick play button");
+      await user.click(playknapper[0]);
+
+      await waitFor(() => {
+        expect(sendLevertInnloggetIaTjeneste).toHaveBeenCalledTimes(1);
+      });
+
+      // sjekk at det ikke sendes flere metrikker ved klikk forskjellige videoer
+      await user.click(playknapper[1]);
+      await waitFor(() => {
+        expect(sendLevertInnloggetIaTjeneste).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    function MockQbrickVideoPlayer() {
+      return (
+        <div>
+          <span>
+            This is a dummy component, and should never be seen outside a code
+            editor or terminal
+          </span>
+          <button
+            onClick={() =>
+              document.dispatchEvent(new CustomEvent("videoAvspilles"))
+            }
+          >
+            mock qbrick play button
+          </button>
+        </div>
+      );
+    }
+
+    const renderNettkurs = () =>
+      render(
+        <VideoOgKurs
+          page={{
+            title: "VideoOgKurs",
+            description:
+              "Her får du informasjon om hvordan du kan forebygge fravær på arbeidsplassen",
+          }}
+          kjørerMockApp={false}
+          grafanaAgentUrl=""
+        />
+      );
+  });
+
+  describe("Forside", () => {
+    it("Kaller sendLevertInnloggetIaTjeneste ved klikk på en lenkeflis", async () => {
+      renderPage();
+
+      const statistikklenke = screen.getByRole("link", {
+        name: "Verktøy for forebygging av sykefravær",
+      });
+
+      expect(sendLevertInnloggetIaTjeneste).toHaveBeenCalledTimes(0);
+
+      await user.click(statistikklenke);
+
+      expect(sendLevertInnloggetIaTjeneste).toHaveBeenCalledTimes(1);
+    });
+
+    function renderPage() {
+      const kjørerMockApp = false;
+      render(
+        <Home
+          page={{
+            title: "Home",
+            description: "noe",
+          }}
+          forsideProps={{
+            samtalestøtteUrl: "samtalestøtteUrl",
+            forebyggingsplanUrl: "forebyggingsplanUrl",
+            sykefraværsstatistikkUrl: "sykefraværsstatistikkUrl",
+            kontaktOssUrl: "kontaktOssUrl",
+            kjørerMockApp,
+          }}
+          minSideArbeidsgiverUrl="minSideArbeidsgiverUrl"
+          kjørerMockApp={kjørerMockApp}
+          grafanaAgentUrl="grafanaAgentUrl"
+        />
+      );
+    }
+  });
+});
