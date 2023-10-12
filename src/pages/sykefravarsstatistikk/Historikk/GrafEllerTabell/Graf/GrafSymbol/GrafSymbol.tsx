4ae229f474a693eeb4a6239f086563e982183e7d
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-10-12 08:59:59 +0200

    Disablet server side rendering for recharts.

--- src/pages/sykefravarsstatistikk/Historikk/GrafEllerTabell/Graf/GrafSymbol/GrafSymbol.tsx
+++ src/pages/sykefravarsstatistikk/Historikk/GrafEllerTabell/Graf/GrafSymbol/GrafSymbol.tsx
@@ -1,19 +1,22 @@
-import React, { FunctionComponent } from 'react';
-import { getFarge, getSymbol } from '../graf-utils';
-import SymbolSvg from './SymbolSvg';
-import classNames from 'classnames';
-import { HistorikkLabel } from '../../../../utils/sykefraværshistorikk-utils';
+import React, { FunctionComponent } from "react";
+import classNames from "classnames";
+import dynamic from "next/dynamic";
+
+import { getFarge, getSymbol } from "../graf-utils";
+import { HistorikkLabel } from "../../../../utils/sykefraværshistorikk-utils";
+
+const SymbolSvg = dynamic(() => import("./SymbolSvg"), { ssr: false });
 
 interface Props {
-    linje: HistorikkLabel;
-    className?: string;
+  linje: HistorikkLabel;
+  className?: string;
 }
 
 export const GrafSymbol: FunctionComponent<Props> = ({ linje, className }) => (
-    <SymbolSvg
-        size={18}
-        symbolType={getSymbol(linje)}
-        fill={getFarge(linje)}
-        className={classNames(className)}
-    />
+  <SymbolSvg
+    size={18}
+    symbolType={getSymbol(linje)}
+    fill={getFarge(linje)}
+    className={classNames(className)}
+  />
 );
