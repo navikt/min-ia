1e7c02e6562a59d513f3a8d273c349e31bd088de
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-11-23 09:44:47 +0100

    Fjern ubrukt _app.tsx

--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "../styles/globals.scss";
-import type {AppProps} from "next/app";
-
-export default function MyApp({Component, pageProps}: AppProps) {
-    return <Component {...pageProps}/>;
-}
