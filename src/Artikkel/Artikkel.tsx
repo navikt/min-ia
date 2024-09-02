0c9713651590dcde2c5117cfcf335cdb36188275
Thomas Dufourd <thomas.dufourd@gmail.com>
2024-09-02 17:15:35 +0200

    Lenke til artikkel om rekruttering

---
+++ src/Artikkel/Artikkel.tsx
@@ -0,0 +1,34 @@
+import styles from "./Artikkel.module.scss";
+import { BodyLong, Heading, Link } from "@navikt/ds-react";
+import Image from "next/image";
+import artikkelBildeLiten from "./artikkelBilde_liten.png"
+import { ExternalLinkIcon } from '@navikt/aksel-icons';
+
+
+import React from "react";
+
+export function Artikkel() {
+    const lenkeTil = "https://arbeidsgiver.nav.no/min-side-arbeidsgiver/artikkel/saferoad_ansetter_ved_hjelp_av_nav"
+
+    return (
+        <div className={styles.lenkeflis}>
+            <div className={styles.lenkeflisInnhold}>
+                <Image
+                    className={styles.lenkeflisBilde}
+                    src={artikkelBildeLiten}
+                    width={50}
+                    height={50}
+                    alt="illustrasjon"
+                    layout="responsive"
+                />
+                <div className={styles.lenkeflisInnholdTittelOgTekst}>
+                    <Heading level="3" size="medium">Ønsker du hjelp til å rekruttere?</Heading>
+                    <BodyLong>
+                        <Link href={lenkeTil}>Les om hvordan NAV bidrar til at Saferoad kan ansette over 100 nye folk
+                            hvert år. <ExternalLinkIcon title="Lenke til artikkel i ny side" fontSize="1.5rem"/> </Link>
+                    </BodyLong>
+                </div>
+            </div>
+        </div>
+    );
+}
