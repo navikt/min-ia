b6e5c17d45ade3c35a1f5a5287b89505d09ed69c
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-03-07 15:41:16 +0100

    Legge til log i handler (page router /api)

--- src/pages/api/authenticated/sykefravarsstatistikk/aggregert.ts
+++ src/pages/api/authenticated/sykefravarsstatistikk/aggregert.ts
@@ -1,11 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { erGyldigOrgnr } from "../../../../hooks/useOrgnr";
 import proxyRequestWithTokenExchange from "../../../../utils/api-proxy";
+import {backendLogger} from "../../../../utils/backendLogger";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
+    req: NextApiRequest,
+    res: NextApiResponse,
 ) {
+  backendLogger.info(`[DEBUG] mottok request på endepunkt /api/authenticated/sykefravarsstatistikk/aggregert`);
   if (req.method !== "GET")
     return res.status(405).json({ error: "Method Not Allowed" });
   if (!req.query.orgnr)
