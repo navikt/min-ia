5a7606104d76351d1fe1f8b2ef8a844155307b2a
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-03-13 14:22:23 +0100

    Fjerner unødvendig loggmelding
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>
    Co-authored-by: Sindre Larsen <sindre.larsen@nav.no>
    Co-authored-by: Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>

--- src/utils/api-proxy.ts
+++ src/utils/api-proxy.ts
@@ -11,7 +11,6 @@ export default async function proxyRequestWithTokenExchange(
   audience: string | undefined,
   useHttps: boolean,
 ) {
-  backendLogger.info(`proxyRequestWithTokenExchange med audience '${audience}', for hostname '${hostname}' og path '${path}'`);
 
   if (audience === undefined) {
     backendLogger.error("audience is not set");
@@ -23,8 +22,6 @@ export default async function proxyRequestWithTokenExchange(
   if (isInvalidToken(newAuthToken)) {
       backendLogger.info(`Token er ikke gyldig. Kan skyldes at bruker ikke er innlogget, token har gått ut eller feil ved verifisering av tokenet. Årsak: ${newAuthToken.errorType}`);
     return res.status(401).json({ error: "authentication failed" });
-  } else {
-    backendLogger.info(`proxyRequestWithTokenExchange, token er valid`);
   }
 
   await proxyApiRouteRequest({
