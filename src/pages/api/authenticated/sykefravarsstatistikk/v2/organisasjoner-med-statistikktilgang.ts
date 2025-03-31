5ffb1bf460ed9e9fee3830dd69173290bb22fc56
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-03-31 12:46:32 +0200

    Legge til API /v2/organisasjoner endepunktene som kaller pia-sykefraværsstatistikk

---
+++ src/pages/api/authenticated/sykefravarsstatistikk/v2/organisasjoner-med-statistikktilgang.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import proxyRequestWithTokenExchange from "../../../../../utils/api-proxy";
+import {backendLogger} from "../../../../../utils/backendLogger";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET")
+    return res.status(405).json({ error: "Method Not Allowed" });
+
+  const erIProd = !(process.env.NAIS_CLUSTER_NAME === "dev-gcp" || process.env.NODE_ENV === "development");
+  if (erIProd) {
+    backendLogger.warn(`Endepunkt /v2/organisasjoner-med-statistikktilgang er ikke tilgjengelig i cluster '${process.env.NAIS_CLUSTER_NAME}' eller NODE_ENV '${process.env.NODE_ENV}'`);
+    return res.status(400).json({
+      error: "Endepunkt er ikke tilgjengelig"
+    });
+  }
+
+  return await proxyRequestWithTokenExchange(
+    req,
+    res,
+    `${process.env.PIA_SYKEFRAVARSSTATISTIKK_HOSTNAME}`,
+    "/sykefravarsstatistikk/organisasjoner/enkeltrettighet",
+    process.env.PIA_SYKEFRAVARSSTATISTIKK_AUDIENCE,
+    true
+  );
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
