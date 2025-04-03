7615f7292dffe9eb0f2f935ad6a76ce785ac0822
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-04-03 16:02:03 +0200

    Fjerne unødvendig log

--- src/hooks/useApiPath.ts
+++ src/hooks/useApiPath.ts
@@ -2,7 +2,6 @@ export const useApiPath = (v2Path: string, defaultPath: string): string => {
     let erV2 = false;
     if (typeof window !== 'undefined') {
         erV2 = localStorage.getItem("erV2") === "true";
-        console.log(`[DEBUG] erV2: ${erV2}`);
     }
 
     return erV2 ? v2Path : defaultPath;
