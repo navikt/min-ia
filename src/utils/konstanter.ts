cc3bbb5b9345fb4e206166d61c371ea7e6b2a319
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-05-06 14:58:11 +0200

    La til noen lenker i konster.

--- src/utils/konstanter.ts
+++ src/utils/konstanter.ts
@@ -3,4 +3,8 @@ export const API_BASE_PATH_V2 = "/forebygge-fravar/api/authenticated/sykefravars
 export const AUTHENTICATED_BASE_PATH = API_BASE_PATH + "/authenticated";
 export const SYKEFRAVARSSTATISTIKK_BASE_PATH =
   AUTHENTICATED_BASE_PATH + "/sykefravarsstatistikk";
-export const SAMTALESTØTTE_URL = "https://www.nav.no/arbeidsgiver/samtalestotte-arbeidsgiver"
+export const SAMTALESTØTTE_URL = "https://www.nav.no/arbeidsgiver/samtalestotte-arbeidsgiver";
+export const ARBEIDSMILJØPORTALEN_URL = "https://www.arbeidsmiljoportalen.no";
+export const IDEBANKEN_URL = "https://www.idebanken.org";
+export const IA_REGJERINGEN_URL = "https://www.regjeringen.no/no/tema/arbeidsliv/arbeidsmiljo-og-sikkerhet/inkluderende_arbeidsliv/";
+export const EKSPERTBISTAND_URL = "https://www.nav.no/arbeidsgiver/ekspertbistand";
