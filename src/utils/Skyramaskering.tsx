d684f5cb05a66537bfd8818fa17458c27d894ac8
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-06-25 15:38:09 +0200

    Fjernet unødvendige logger.

--- src/utils/Skyramaskering.tsx
+++ src/utils/Skyramaskering.tsx
@@ -16,13 +16,10 @@ export default function Skyramaskering() {
 	// Vi trenger å kjøre redactSearchParam for å fjerne bedrift fra URL-en om bruker svarer på Skyra-undersøkelsen.
 	React.useEffect(() => {
 		if (retryCount < MAX_RETRY_ATTEMPTS) {
-			console.log("Prøver maskering, retrtyCount:", retryCount);
 			if (window !== undefined && window?.skyra?.redactSearchParam) {
 				window?.skyra?.redactSearchParam("bedrift", { path: "/forebygge-fravar" });
-				console.log("Skyramaskering: redactSearchParam called successfully.");
 			} else {
 				setTimeout(() => {
-					console.warn("Skyramaskering: window is not defined, retrying...");
 					setRetryCount((prevCount) => prevCount + 1);
 				}, 1000); // Retry after 1 second
 			}
