b732281e0fb329a301146d4c7fc75b742aeff487
Christian Klem <christian.klem@nav.no>
2025-08-12 13:14:32 +0200

    Legg til endepunkt for å hente samarbeid fra fia-arbeidsgiver
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>
    Co-authored-by: Sindre Larsen <sindre.larsen@nav.no>

---
+++ src/pages/api/authenticated/fia-samarbeid.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { erGyldigOrgnr } from "../../../hooks/useOrgnr";
+import proxyRequestWithTokenExchange from "../../../utils/api-proxy";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== "GET")
+        return res.status(405).json({ error: "Method Not Allowed" });
+    if (!req.query.orgnr)
+        return res.status(400).json({ error: "Mangler parameter 'orgnr'" });
+
+    const orgnr = req.query.orgnr as string;
+    if (!erGyldigOrgnr(orgnr)) {
+        return res.status(400).json({ error: "Ugyldig orgnr" });
+    }
+
+    return await proxyRequestWithTokenExchange(
+        req,
+        res,
+        `${process.env.FIA_ARBEIDSGIVER_HOSTNAME}`,
+        `/api/samarbeid/${orgnr}`,
+        process.env.FIA_ARBEIDSGIVER_AUDIENCE,
+        false
+    );
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+        externalResolver: true,
+    },
+};
