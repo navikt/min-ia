7a22def11b7b7a891667eba2a2c23c516500f3e0
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-08-21 08:33:56 +0200

    La til tester for samarbeidsvelger

---
+++ src/Samarbeid/Samarbeidsvelger/Samarbeidsvelger.test.tsx
@@ -0,0 +1,125 @@
+import React from "react";
+import Samarbeidsvelger from ".";
+import { SamarbeidsvelgerProvider } from "./SamarbeidsvelgerContext";
+import { render, screen, waitFor } from "@testing-library/react";
+import { OrgnrProvider } from "../../utils/OrgnrContext";
+import { fiaSamarbeidMock } from "../../local/fia-samarbeidMock";
+import { axe } from "jest-axe";
+import { RestStatus } from "../../integrasjoner/rest-status";
+import { penskrivIAStatus } from "../SamarbeidsStatusBadge";
+import { SamarbeidStatus } from "./samarbeidtyper";
+
+const mockdata = fiaSamarbeidMock().map((samarbeid) => ({
+	...samarbeid,
+	id: `${samarbeid.id}`,
+}));
+
+jest.mock("../fiaSamarbeidAPI", () => ({
+	useFiaSamarbeid: jest.fn(() => ({
+		status: RestStatus.Suksess,
+		data: mockdata,
+	})),
+}));
+
+describe("Samarbeidsvelger", () => {
+	it("Render uten å krasje", () => {
+		let valgtSamarbeid = `${mockdata[0].id}`;
+
+		const { container } = render(
+			<OrgnrProvider>
+				<SamarbeidsvelgerProvider samarbeidId={valgtSamarbeid} setSamarbeidId={(id: string) => {
+					valgtSamarbeid = id;
+				}}>
+					<Samarbeidsvelger />
+				</SamarbeidsvelgerProvider>
+			</OrgnrProvider>
+		);
+		expect(container).toBeInTheDocument();
+	});
+
+	it("Endrer verdi ved klikk", async () => {
+		let valgtSamarbeid = mockdata[0].id;
+
+		const { container } = render(
+			<OrgnrProvider>
+				<SamarbeidsvelgerProvider samarbeidId={valgtSamarbeid} setSamarbeidId={(id: string) => {
+					valgtSamarbeid = id;
+				}}>
+					<Samarbeidsvelger />
+				</SamarbeidsvelgerProvider>
+			</OrgnrProvider>
+		);
+		expect(container).toBeInTheDocument();
+
+		const knapper = container.querySelectorAll("button");
+		expect(knapper.length).toBeGreaterThan(0);
+		expect(knapper[0]).toBeInTheDocument();
+		expect(knapper[0].textContent).toBe(mockdata[0].navn);
+
+		knapper[0].click();
+		const samarbeid2 = await waitFor(() => screen.getByText(mockdata[2].navn));
+
+		expect(samarbeid2).toBeInTheDocument();
+
+		samarbeid2.click();
+
+		expect(valgtSamarbeid).toBe(mockdata[2].id);
+		expect(screen.getByText(mockdata[2].navn)).toBeInTheDocument();
+	});
+
+	it("Viser riktig status for samarbeid", async () => {
+		let valgtSamarbeid = ``;
+		const { container } = render(
+			<OrgnrProvider>
+				<SamarbeidsvelgerProvider samarbeidId={valgtSamarbeid} setSamarbeidId={(id: string) => {
+					valgtSamarbeid = id;
+				}}>
+					<Samarbeidsvelger />
+				</SamarbeidsvelgerProvider>
+			</OrgnrProvider>
+		);
+
+		expect(container).toBeInTheDocument();
+
+		const knapper = container.querySelectorAll("button");
+		expect(knapper.length).toBeGreaterThan(0);
+		knapper[0].click();
+
+		const samarbeid1 = await waitFor(() => screen.getByRole("menuitem", { name: `${mockdata[0].navn} ${penskrivIAStatus(mockdata[0].status as SamarbeidStatus)}` }));
+		expect(samarbeid1).toBeInTheDocument();
+
+		for (const samarbeid of mockdata) {
+			const status = penskrivIAStatus(samarbeid.status as SamarbeidStatus);
+			expect(screen.getByRole("menuitem", { name: `${samarbeid.navn} ${status}` })).toBeInTheDocument();
+		}
+
+	});
+
+	it("Ingen UU-feil fra axe", async () => {
+		let valgtSamarbeid = `${mockdata[0].id}`;
+
+		const { container } = render(
+			<OrgnrProvider>
+				<SamarbeidsvelgerProvider samarbeidId={valgtSamarbeid} setSamarbeidId={(id: string) => {
+					valgtSamarbeid = id;
+				}}>
+					<Samarbeidsvelger />
+				</SamarbeidsvelgerProvider>
+			</OrgnrProvider>
+		);
+		expect(container).toBeInTheDocument();
+
+		//uten åpen meny
+		expect(await axe(container)).toHaveNoViolations();
+
+		const knapper = container.querySelectorAll("button");
+		expect(knapper.length).toBeGreaterThan(0);
+		knapper[0].click();
+
+		const samarbeid1 = await waitFor(() => screen.getByRole("menuitem", { name: `${mockdata[0].navn} ${penskrivIAStatus(mockdata[0].status as SamarbeidStatus)}` }));
+		expect(samarbeid1).toBeInTheDocument();
+
+		//med åpen meny
+		expect(await axe(container)).toHaveNoViolations();
+	});
+});
