164dfb8f2b2f19abfb54cbe5eb47a80623e4224f
Sindre Larsen <silar.dev@proton.me>
2025-08-25 14:25:55 +0200

    Legger til dokumentId i query param for uthenting av dokument
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Brage Hagemann Brataas <Brage.Hagemann.Brataas@nav.no>

--- src/pages/api/authenticated/fia-dokument.ts
+++ src/pages/api/authenticated/fia-dokument.ts
@@ -4,25 +4,29 @@ import proxyRequestWithTokenExchange from "../../../utils/api-proxy";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (req.method !== "GET")
     return res.status(405).json({ error: "Method Not Allowed" });
   if (!req.query.orgnr)
     return res.status(400).json({ error: "Mangler parameter 'orgnr'" });
+  if (!req.query.dokumentId)
+    return res.status(400).json({ error: "Mangler parameter 'dokumentId'" });
 
   const orgnr = req.query.orgnr as string;
+  const dokumentId = req.query.dokumentId as string;
   if (!erGyldigOrgnr(orgnr)) {
     return res.status(400).json({ error: "Ugyldig orgnr" });
   }
+  //TODO: Er gyldig dokumentId?
 
   return await proxyRequestWithTokenExchange(
     req,
     res,
     `${process.env.FIA_ARBEIDSGIVER_HOSTNAME}`,
-    `/fia-arbeidsgiver/dokument/${orgnr}`,
+    `/fia-arbeidsgiver/dokument/${orgnr}/${dokumentId}`,
     process.env.FIA_ARBEIDSGIVER_AUDIENCE,
-    false
+    false,
   );
 }
 
