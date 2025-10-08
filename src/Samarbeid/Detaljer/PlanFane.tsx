8b09a6b157eb3b6d077bcfb9eed4fb8fb2438cb1
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-10-08 15:57:39 +0200

    La til noen tester for PlanFane. Flyttet PlanFane inn i egen mappe.

--- src/Samarbeid/Detaljer/PlanFane.tsx
+++
@@ -1,6 +0,0 @@
-import Plan from "../../komponenter/Plan";
-import { dummyplan } from "../../komponenter/Plan/dummydata";
-
-export default function PlanFane() {
-	return <Plan plan={dummyplan} />;
-}
---
+++ src/Samarbeid/Detaljer/PlanFane/index.tsx
@@ -0,0 +1,6 @@
+import Plan from "../../../komponenter/Plan";
+import { dummyplan } from "../../../komponenter/Plan/dummydata";
+
+export default function PlanFane() {
+	return <Plan plan={dummyplan} />;
+}
