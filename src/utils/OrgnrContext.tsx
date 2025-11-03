020199d498bc5ac4761e7f012acfe4645e4deabe
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-11-03 10:31:58 +0100

    Setter orgnr i context fra URL og fjerner fra URL.

--- src/utils/OrgnrContext.tsx
+++ src/utils/OrgnrContext.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 import React from "react";
 
 export const OrgnrContext = React.createContext<{
@@ -11,7 +13,25 @@ export const OrgnrProvider = ({
 }: {
 	children: React.ReactNode;
 }) => {
+	const router = useRouter();
+	const searchParams = useSearchParams();
 	const [orgnr, setOrgnr] = React.useState<string | null>(null);
+
+	React.useEffect(() => {
+		if (typeof router.query.bedrift === 'string') {
+			const nextSearchParams = new URLSearchParams(searchParams.toString());
+
+			if (nextSearchParams.size > 1) {
+				nextSearchParams.delete('bedrift');
+				setOrgnr(router.query.bedrift);
+				router.replace(router.pathname, `?${nextSearchParams.toString()}`, { shallow: true, scroll: false });
+			} else {
+				setOrgnr(router.query.bedrift);
+				router.replace(router.pathname, undefined, { shallow: true, scroll: false });
+			}
+		}
+	}, [router.query.bedrift, router, searchParams]);
+
 	return (
 		<OrgnrContext.Provider value={{ orgnr, setOrgnr }}>
 			{children}
