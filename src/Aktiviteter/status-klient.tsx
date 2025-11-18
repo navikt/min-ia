db550a5c38ede50a08776b66f117ac4b6b5627f4
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-11-18 10:37:47 +0100

    Sjekker at orgnr er ekte (og ikke inneholder noe skummelt) før vi bruker det i URL.

--- src/Aktiviteter/status-klient.tsx
+++ src/Aktiviteter/status-klient.tsx
@@ -1,11 +1,16 @@
 import { StatusType } from "./AktivitetData";
-import {API_BASE_PATH} from "../utils/konstanter";
+import { API_BASE_PATH } from "../utils/konstanter";
+import { erGyldigOrgnr } from "../hooks/useOrgnr";
 
 export const oppdaterStatus = async (
   aktivitetId: string,
   orgnr: string,
   status: StatusType
 ) => {
+  if (!erGyldigOrgnr(orgnr)) {
+    throw new Error("Ugyldig orgnr ved oppdatering av aktivitetstatus");
+  }
+
   return await fetch(
       `${API_BASE_PATH}/aktivitet/${aktivitetId}/orgnr/${orgnr}/oppdater`,
     {
