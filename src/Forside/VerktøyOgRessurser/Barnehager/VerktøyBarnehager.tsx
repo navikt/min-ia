1b41b70c1b93ac0f1a74233cf199aacae4f762b4
Brage.Hagemann.Brataas <brage.hagemann.brataas@nav.no>
2025-12-12 09:52:32 +0100

    Legg til bilder på nye verktøy for barnehager

--- src/Forside/VerktøyOgRessurser/Barnehager/VerktøyBarnehager.tsx
+++ src/Forside/VerktøyOgRessurser/Barnehager/VerktøyBarnehager.tsx
@@ -6,6 +6,11 @@ import styles from "./verktøyBarnehager.module.scss";
 import { IDEBANKEN_URL } from "../../../utils/konstanter";
 import { sendNavigereEvent } from "../../../utils/analytics/analytics";
 
+import Arbeidsmiljøhjelpen from "./Arbeidsmiljohjelpen.png"
+import En_Bra_Dag_På_Jobb from "./En_bra_dag_paa_jobb.png"
+import Idebanken_Barnehage from "./Idebanken_Barnehage.png"
+import Image, { StaticImageData } from "next/image";
+
 const ARBEIDSMILJØHJELPEN_BARNEHAGE_URL = "https://arbeidsmiljohjelpen.arbeidstilsynet.no/bransje/barnehage/";
 const EN_BRA_DAG_PÅ_JOBB_BARNEHAGE_URL = "https://enbradagpajobb.no/bransje/barnehage-2/"
 const IDEBANKEN_BARNEHAGE_URL = IDEBANKEN_URL + "/ia/ia-hjelp/ia-bransjeprogram/barnehage"
@@ -22,18 +27,27 @@ export default function VerktøyBarnehager() {
           innhold="Diskuter typiske utfordringer i bransjen og lag en handlingsplan for å utvikle arbeidsmiljøet"
           lenke={ARBEIDSMILJØHJELPEN_BARNEHAGE_URL}
           lenketekst="Arbeidsmiljøhjelpen"
+          bilde={Arbeidsmiljøhjelpen}
+          bildeClassName={styles.arbeidsmiljøhjelpenBilde}
+          kortClassName={styles.arbeidsmiljøhjelpenKort}
         />
         <VerktøykortBarnehager
           tittel="En bra dag på jobb"
-          innhold="Et verktøy for å skape god arbeidsflyt, øke ansattes motivasjon og redusere sykefraværet"
+          innhold="Gjennom korte filmer og enkle diskusjonsopplegg får dere et felles grunnlag for å kartlegge utfordringer, prioritere tiltak og lage en handlingsplan"
           lenke={EN_BRA_DAG_PÅ_JOBB_BARNEHAGE_URL}
           lenketekst="En bra dag på jobb"
+          bilde={En_Bra_Dag_På_Jobb}
+          bildeClassName={styles.enBraDagBilde}
+          kortClassName={styles.enBraDagKort}
         />
         <VerktøykortBarnehager
           tittel="IA-bransjeprogram"
           innhold="Finn bransjetilpassede verktøy og tiltak for å få ned sykefraværet og hindre frafall på din arbeidsplass"
           lenke={IDEBANKEN_BARNEHAGE_URL}
           lenketekst="Idébanken"
+          bilde={Idebanken_Barnehage}
+          bildeClassName={styles.idebankenBilde}
+          kortClassName={styles.idebankenKort}
         />
       </div>
     </Page.Block>
@@ -45,14 +59,20 @@ function VerktøykortBarnehager({
   innhold,
   lenke,
   lenketekst,
+  bilde,
+  bildeClassName,
+  kortClassName,
 }: {
   tittel: ReactNode;
   innhold: ReactNode;
   lenke: string;
   lenketekst: string;
+  bilde: StaticImageData;
+  bildeClassName: string;
+  kortClassName: string;
 }) {
   return (
-    <div className={styles.verktøykort}>
+    <div className={`${styles.verktøykort} ${kortClassName}`}>
       <Heading size="small" level="3">
         {tittel}
       </Heading>
@@ -73,6 +93,7 @@ function VerktøykortBarnehager({
       >
         {lenketekst}
       </Button>
+      <Image src={bilde} alt={lenketekst} className={bildeClassName}/>
     </div>
   );
 }
