0532d35712eaddad7c916bbc97cb4d85b19e5f2f
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-12-18 10:50:18 +0100

    Oppgraderer fra next lint til eslint. Endret config (AI-assistert) til å funke med ny setup.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,54 +1,60 @@
-import { fixupConfigRules } from "@eslint/compat";
-import { FlatCompat } from '@eslint/eslintrc';
 import js from "@eslint/js";
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended
-})
+import next from "eslint-config-next";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+import jestPlugin from "eslint-plugin-jest";
 
-const eslingconfig = [
-	...compat.config(
-		...fixupConfigRules(
-			{
-				extends: [
-					"eslint:recommended",
-					"plugin:@typescript-eslint/eslint-recommended",
-					"plugin:@typescript-eslint/recommended",
-					"plugin:jest/recommended",
-					"next/core-web-vitals",
-					"prettier", // Add "prettier" last. This will turn off eslint rules conflicting with prettier. This is not what will format our code.
-				],
-				rules: {
-					"@typescript-eslint/no-unused-vars": "error",
-					"@typescript-eslint/no-empty-function": "off",
-					"@typescript-eslint/ban-ts-comment": "off",
-					"jsx-a11y/anchor-ambiguous-text": [
-					2,
-					{
-						words: [
-						"her",
-						"klikk",
-						"klikk her",
-						"trykk",
-						"trykk her",
-						"lenken",
-						"linken",
-						"lenka",
-						"lenken her",
-						"linken her",
-						"lenka her",
-						"denne lenken",
-						"denne linken",
-						"denne lenka",
-						],
-					},
-					],
-				},
-				settings: {},
-				ignorePatterns: ["*.module.css", "*.{config,setup}.{ts,js}", "dist/*", "node_modules/*"],
-			}
-		),
-	)
+const eslintConfig = [
+    // Base JS recommendations from ESLint
+    js.configs.recommended,
+
+    // Next.js and TypeScript presets
+    ...next,
+    ...nextCoreWebVitals,
+    ...nextTypescript,
+
+    // Project-specific overrides
+    {
+        plugins: {
+            jest: jestPlugin,
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": "error",
+            "@typescript-eslint/no-empty-function": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+            "jsx-a11y/anchor-ambiguous-text": [
+                2,
+                {
+                    words: [
+                        "her",
+                        "klikk",
+                        "klikk her",
+                        "trykk",
+                        "trykk her",
+                        "lenken",
+                        "linken",
+                        "lenka",
+                        "lenken her",
+                        "linken her",
+                        "lenka her",
+                        "denne lenken",
+                        "denne linken",
+                        "denne lenka",
+                    ],
+                },
+            ],
+        },
+        ignores: [
+            "node_modules/**",
+            ".next/**",
+            "out/**",
+            "build/**",
+            "next-env.d.ts",
+            "*.module.css",
+            "*.{config,setup}.{ts,js}",
+            "dist/**",
+        ],
+    },
 ];
 
-export default eslingconfig;
+export default eslintConfig;
