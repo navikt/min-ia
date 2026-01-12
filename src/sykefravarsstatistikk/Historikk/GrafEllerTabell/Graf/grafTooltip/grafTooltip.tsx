a541fb6ad7cdd69615b4ba1bbe273321b2d55867
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2026-01-12 15:15:06 +0100

    Fikser endret type fra recharts.

--- src/sykefravarsstatistikk/Historikk/GrafEllerTabell/Graf/grafTooltip/grafTooltip.tsx
+++ src/sykefravarsstatistikk/Historikk/GrafEllerTabell/Graf/grafTooltip/grafTooltip.tsx
@@ -7,7 +7,7 @@ import { HistorikkLabel } from "../../../../utils/sykefraværshistorikk-utils";
 
 const grafTooltip = () => (
   <Tooltip
-    formatter={(value: number, name: HistorikkLabel) => [
+    formatter={(value: number | undefined, name: HistorikkLabel | undefined) => [
       <span className={styles["tooltip__item-wrapper"]} key={`tooltip-${name}`}>
         <GrafSymbol linje={name} className={styles["tooltip__ikon"]} />
         <div className={styles["tooltip__item-navn-og-verdi"]}>
